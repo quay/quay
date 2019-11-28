6600dbd65a646ca7e3b9d2c232c543b5c6b37f8e
Maor Friedman <maor.friedman@redhat.com>
2019-11-28 13:03:33 +0200

    use docker config file

--- scripts/app_sre_post_deploy_rhel7.sh
+++ scripts/app_sre_post_deploy_rhel7.sh
@@ -4,7 +4,8 @@
 
 set -exv
 
-CURRENT_DIR=$(dirname $0)
+export DOCKER_CONF="$PWD/.docker"
+mkdir -p "${DOCKER_CONF}"
 
 BASE_IMG="quay"
 IMG="${BASE_IMG}:latest"
@@ -12,13 +13,18 @@ IMG="${BASE_IMG}:latest"
 GIT_HASH=`git rev-parse --short=7 HEAD`
 
 # login to the backup repository
-aws ecr get-login --region ${AWS_REGION} --no-include-email | /bin/bash
+aws ecr get-login \
+    --region ${AWS_REGION} --no-include-email | \
+    sed 's/docker/docker --config="$DOCKER_CONF"/g' | \
+    /bin/bash
 
 # push the image
 skopeo copy \
+    --authfile "$DOCKER_CONF/config.json" \
     "docker-daemon:${IMG}" \
     "docker://${BACKUP_REPO_URL}:latest"
 
 skopeo copy \
+    --authfile "$DOCKER_CONF/config.json" \
     "docker-daemon:${IMG}" \
     "docker://${BACKUP_REPO_URL}:${GIT_HASH}"
