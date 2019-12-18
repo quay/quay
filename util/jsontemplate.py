a563ec186f5c952a4e507137871fb9019d66932d
Joseph Schorr <joseph.schorr@coreos.com>
2019-12-18 15:37:05 +0200

    Add support for custom webhook bodies
    This will allow users to configure the body content of outgoing webhooks for notifications
    
    Fixes https://jira.coreos.com/browse/QUAY-1322

---
+++ util/jsontemplate.py
@@ -0,0 +1,81 @@
+import json
+import re
+
+from jsonpath_rw import parse as parse_json_path
+from jsonpath_rw.lexer import JsonPathLexerError
+
+INLINE_PATH_PATTERN = r"\$\{([^}]*)\}"
+
+
+class JSONTemplateParseException(Exception):
+    """ Exception raised if a JSON template could not be parsed. """
+
+
+class JSONTemplate(object):
+    """ Represents a parsed template for producing JSON. """
+
+    def __init__(self, template_string):
+        try:
+            self._parsed = json.loads(template_string)
+        except ValueError as ve:
+            raise JSONTemplateParseException("Could not parse template: %s" % ve)
+
+        # Apply against an empty object to validate.
+        self.apply({})
+
+    def apply(self, data):
+        return self._apply(self._parsed, data)
+
+    def _apply(self, obj, data):
+        if isinstance(obj, basestring):
+            return self._process_string(obj, data)
+        elif isinstance(obj, dict):
+            return {
+                self._process_string(key, data): self._apply(value, data)
+                for key, value in obj.iteritems()
+            }
+        elif isinstance(obj, list):
+            return [self._apply(item, data) for item in obj]
+        else:
+            return obj
+
+    def _process_string(self, str_value, data):
+        # Check for a direct match first.
+        if re.match("^" + INLINE_PATH_PATTERN + "$", str_value):
+            expression = str_value[2:-1]
+            return self._process_inline(expression, data)
+
+        def process_inline(match):
+            result = self._process_inline(match.group(1), data)
+            if result is None:
+                return "(none)"
+
+            if isinstance(result, list) and len(result) > 1:
+                return ",".join(result)
+
+            return str(result)
+
+        return re.sub(INLINE_PATH_PATTERN, process_inline, str_value)
+
+    def _process_inline(self, expression, data):
+        if not expression:
+            raise JSONTemplateParseException("Empty expression found")
+
+        try:
+            parsed = parse_json_path(expression)
+        except JsonPathLexerError as jple:
+            raise JSONTemplateParseException("For expression `%s`: %s" % (expression, jple))
+        except Exception as ex:
+            raise JSONTemplateParseException("For expression `%s`: %s" % (expression, ex))
+
+        try:
+            found = parsed.find(data)
+            if not found:
+                return None
+
+            if len(found) > 1:
+                return [f.data for f in found]
+
+            return found[0].value
+        except IndexError:
+            return None
---
+++ util/test/test_jsontemplate.py
@@ -0,0 +1,76 @@
+import pytest
+
+from util.jsontemplate import JSONTemplate, JSONTemplateParseException
+
+
+@pytest.mark.parametrize(
+    "template_string, data, expected",
+    [
+        ("{}", {}, {}),
+        ('{"hello": "world"}', {}, {"hello": "world"}),
+        pytest.param(
+            '{"hello": "${thing}"}', {"thing": 1234}, {"hello": 1234}, id="direct expression"
+        ),
+        pytest.param(
+            '{"hello": "cool-${thing}"}',
+            {"thing": "beans"},
+            {"hello": "cool-beans"},
+            id="inline expression",
+        ),
+        pytest.param(
+            '{"hello": "${first.second.third}"}',
+            {"first": {"second": {"third": 42}}},
+            {"hello": 42},
+            id="nested object",
+        ),
+        pytest.param(
+            '{"hello": "${first.second.third} is the answer"}',
+            {"first": {"second": {"third": 42}}},
+            {"hello": "42 is the answer"},
+            id="nested object with inline expression",
+        ),
+        pytest.param(
+            '{"hello": "${first.doesnotexist.third}"}',
+            {"first": {"second": {"third": 42}}},
+            {"hello": None},
+            id="inline expression with unknown child",
+        ),
+        pytest.param(
+            '{"hello": "${first[2]}"}', {"first": [1, 2, 42]}, {"hello": 42}, id="array access"
+        ),
+        pytest.param(
+            '{"hello": "${first[2]}"}',
+            {"first": [1, 2]},
+            {"hello": None},
+            id="outside of array index",
+        ),
+        pytest.param(
+            '{"hello": ["${first[1]}", "${first[0]}"]}',
+            {"first": [1, 2]},
+            {"hello": [2, 1]},
+            id="array to list",
+        ),
+        pytest.param(
+            '{"hello": "hey-${first[2]}"}',
+            {"first": [1, 2]},
+            {"hello": "hey-(none)"},
+            id="outside of array index inline expression",
+        ),
+        pytest.param('{"hello": "}', {}, JSONTemplateParseException, id="invalid template"),
+        pytest.param('{"hello": "${}"}', {}, JSONTemplateParseException, id="empty expression"),
+        pytest.param('{"hello": "${;;}"}', {}, JSONTemplateParseException, id="invalid expression"),
+        pytest.param(
+            '{"hello": "${first[}"}',
+            {},
+            JSONTemplateParseException,
+            id="another invalid expression",
+        ),
+    ],
+)
+def test_json_template(template_string, data, expected):
+    if expected == JSONTemplateParseException:
+        with pytest.raises(JSONTemplateParseException):
+            JSONTemplate(template_string)
+    else:
+        jt = JSONTemplate(template_string)
+        assert jt.apply(data) == expected
