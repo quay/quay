4d36201ceada30b5b9922289d5fc49939a6b5a37
Tom McKay <thomasmckay@redhat.com>
2019-12-18 10:20:22 -0500

    PROJQUAY-96 - python3 Dockerfile and related

--- _init.py
+++ _init.py
@@ -30,7 +30,7 @@ config_provider = get_config_provider(
 
 def _get_version_number_changelog():
     try:
-        with open(os.path.join(ROOT_DIR, "CHANGELOG.md")) as f:
+        with open(os.path.join(ROOT_DIR, "CHANGELOG.md"), "r", encoding="UTF-8") as f:
             return re.search(r"(v[0-9]+\.[0-9]+\.[0-9]+)", f.readline()).group(0)
     except IOError:
         return ""
