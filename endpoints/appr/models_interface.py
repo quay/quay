303940a95f52f7e17e295b4e4f54e97ef4e4dcbf
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2019-12-19 13:11:45 -0500

    Update buildman to use asyncio instead of trollius.

--- endpoints/appr/models_interface.py
+++ endpoints/appr/models_interface.py
@@ -200,7 +200,7 @@ class AppRegistryDataInterface(object):
         repo_name=None,
         analytics_name=None,
         analytics_sample=1,
-        **kwargs
+        **kwargs,
     ):
         """ Logs an action to the audit log. """
         pass
