54af11d919f80a1d404a517cc2a594db6c6d79b8
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2019-12-19 13:11:45 -0500

    Update buildman to use asyncio instead of trollius.

--- endpoints/appr/models_cnr.py
+++ endpoints/appr/models_cnr.py
@@ -94,7 +94,7 @@ class CNRAppModel(AppRegistryDataInterface):
             repo,
             analytics_name=analytics_name,
             analytics_sample=analytics_sample,
-            **metadata
+            **metadata,
         )
 
     def list_applications(
