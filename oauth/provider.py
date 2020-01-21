0c5eeeaf83b131e2460f7603b0160929d22c993f
Christian Clauss <cclauss@me.com>
2020-01-21 18:43:53 +0100

    StandardError --> Exception

--- oauth/provider.py
+++ oauth/provider.py
@@ -455,7 +455,7 @@ class AuthorizationProvider(Provider):
 
             # Catch missing parameters in request
             return self._make_json_error_response("invalid_request")
-        except StandardError as exc:
+        except Exception as exc:
             self._handle_exception(exc)
 
             # Catch all other server errors
