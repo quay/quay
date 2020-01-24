7de62f8c0ad08b82e1cd44fdc6dc3a49ed50d17f
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2020-01-24 15:11:55 -0500

    Small fixes to secret.py

--- util/security/secret.py
+++ util/security/secret.py
@@ -11,7 +11,7 @@ def convert_secret_key(config_secret_key):
     # First try parsing the key as an int.
     try:
         big_int = int(config_secret_key)
-        secret_key = str(bytearray.fromhex("{:02x}".format(big_int)))
+        secret_key = bytearray.fromhex("{:02x}".format(big_int))
     except ValueError:
         pass
 
@@ -23,13 +23,9 @@ def convert_secret_key(config_secret_key):
             pass
 
     if secret_key is None:
-        secret_key = str(bytearray(list(map(ord, config_secret_key))))
+        secret_key = bytearray(list(map(ord, config_secret_key)))
 
     # Otherwise, use the bytes directly.
     assert len(secret_key) > 0
-    print(secret_key)
-
-    # FIXME(alecmerdler): Testing
-    secret_key = b'\xa3l\x9d}%\xa90\xf4\xa5\x86=/\x8d\xc4\n\x83'
 
     return b"".join(itertools.islice(itertools.cycle([bytes([b]) for b in secret_key]), 32))
