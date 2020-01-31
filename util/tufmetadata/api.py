52439cb33868bee96725e6ca742820168e593756
Kurtis Mullins <kmullins@redhat.com>
2020-01-31 11:25:15 -0500

    [PROJQUAY-218] Fix missing CI base image.
    Run `black` to reformat Python files
    
    Build the Dockerfile.cirun to include test depenencies. Use
      that image for running tests.
    
    Enable Flake8. Disable cleaning cache on failure.
    - The cache contains the Docker container used for all tests at
      the present time.

--- util/tufmetadata/api.py
+++ util/tufmetadata/api.py
@@ -278,7 +278,7 @@ class ImplementedTUFMetadataAPI(TUFMetadataAPIInterface):
             TOKEN_VALIDITY_LIFETIME_S,
             self._instance_keys,
         )
-        return {"Authorization": "Bearer %s" % token.decode('ascii')}
+        return {"Authorization": "Bearer %s" % token.decode("ascii")}
 
     def _get(self, gun, metadata_file):
         return self._call(
