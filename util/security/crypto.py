90a6c55664fa34ec3edf25d3f2daad95362d5d28
Kurtis Mullins <kmullins@redhat.com>
2020-02-06 10:19:27 -0500

    Fix usage of Cryptography.Fernet for Python3 (#219)
    - Specifically, this addresses the issue where Byte<->String
      conversions weren't being applied correctly.

--- util/security/crypto.py
+++ util/security/crypto.py
@@ -10,7 +10,14 @@ def encrypt_string(string, key):
     The key must be 32 raw bytes.
     """
     f = Fernet(key)
-    return f.encrypt(string)
+
+    # Fernet() works only on byte objects. Convert the string to bytes.
+    unencrypted_bytes = string.encode()
+    encrypted_bytes = f.encrypt(unencrypted_bytes)
+
+    # Fernet() returns a byte object. Convert it to a string before returning.
+    encrypted_string = encrypted_bytes.decode()
+    return encrypted_string
 
 
 def decrypt_string(string, key, ttl=None):
@@ -20,9 +27,21 @@ def decrypt_string(string, key, ttl=None):
     The key must be 32 raw bytes.
     """
     f = Fernet(key)
+
+    # Fernet() works only on byte objects. Convert the string to bytes before decrypting.
+    encrypted_bytes = string.encode()  # str -> bytes
+
     try:
-        return f.decrypt(str(string), ttl=ttl)
+        decrypted_bytes = f.decrypt(encrypted_bytes, ttl=ttl)
     except InvalidToken:
-        return None
-    except TypeError:
-        return None
+        """
+        From the the Cryptography's library documentation:
+
+        If the token is in any way invalid, this exception is raised.
+        A token may be invalid for a number of reasons: it is older than the
+        ttl, it is malformed, or it does not have a valid signature.
+        """
+        return None  # TODO(kmullins): Shall we log this case? Is it expected?
+
+    decrypted_string = decrypted_bytes.decode()  # bytes -> str
+    return decrypted_string
