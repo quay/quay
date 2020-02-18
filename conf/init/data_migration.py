6d2ca5fd733b487a4590316bfbc45dffe3c11e1f
Joseph Schorr <joseph.schorr@coreos.com>
2020-02-18 15:33:35 -0500

    Make data_migration more Pythonic

--- conf/init/data_migration.py
+++ conf/init/data_migration.py
@@ -1,10 +1,20 @@
 from app import app
 from active_migration import ActiveDataMigration
 
-if not app.config.get("SETUP_COMPLETE", False):
-    print "head"
-else:
-    if ActiveDataMigration is not None:
-        print ActiveDataMigration.alembic_migration_revision
+
+def current_migration():
+    if not app.config.get("SETUP_COMPLETE", False):
+        return "head"
     else:
-        print "head"
+        if ActiveDataMigration is not None:
+            return ActiveDataMigration.alembic_migration_revision
+        else:
+            return "head"
+
+
+def main():
+    print(current_migration())
+
+
+if __name__ == "__main__":
+    main()
