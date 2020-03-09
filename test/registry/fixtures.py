deea30fde529fb54979e50a55e6938d228736e67
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2020-03-09 16:44:04 -0400

    Fix storage tests

--- test/registry/fixtures.py
+++ test/registry/fixtures.py
@@ -42,7 +42,7 @@ def registry_server_executor(app):
 
     def set_supports_direct_download(enabled):
         storage.put_content(
-            ["local_us"], "supports_direct_download", "true" if enabled else "false"
+            ["local_us"], "supports_direct_download", b"true" if enabled else b"false"
         )
         return "OK"
 
