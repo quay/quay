028f57a7e9d535e9d3c09911802901797e444b89
Joseph Schorr <joseph.schorr@coreos.com>
2020-04-03 11:29:54 -0400

    Add an option for running Quay without running database migrations

--- conf/init/data_migration.py
+++ conf/init/data_migration.py
@@ -1,8 +1,13 @@
+import os
+
 from app import app
 from active_migration import ActiveDataMigration
 
 
 def current_migration():
+    if os.getenv("ENSURE_NO_MIGRATION", "").lower() == "true":
+        raise Exception("Cannot call migration when ENSURE_NO_MIGRATION is true")
+
     if not app.config.get("SETUP_COMPLETE", False):
         return "head"
     else:
