ca0b4d3da916518c903ffa5f6cf37efda598c1cb
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2020-05-29 17:12:59 -0400

    add env var for worker multiplier (#400)

--- conf/gunicorn_web.py
+++ conf/gunicorn_web.py
@@ -13,7 +13,7 @@ from util.workers import get_worker_count, get_worker_connections_count
 logconfig = logfile_path(debug=False)
 
 bind = "unix:/tmp/gunicorn_web.sock"
-workers = get_worker_count("web", 2, minimum=2, maximum=32)
+workers = get_worker_count("web", multiplier=2, minimum=2, maximum=32)
 worker_class = "gevent"
 worker_connections = get_worker_connections_count("web")
 pythonpath = "."
