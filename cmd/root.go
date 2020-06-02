2a8cf6b9ad0bf31f3e111ead0b5c4308b02207e3
Jonathan King <jonathan@redhat-laptop.int.tocaconsulting.com>
2020-06-02 12:24:07 -0400

    migrate code to quay namespace

--- cmd/root.go
+++ cmd/root.go
@@ -17,8 +17,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -29,16 +30,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "config-tool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A Configuration Validation Tool for Quay",
+	Long:  `This tool allows Quay users to validate their configuration bundles.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
