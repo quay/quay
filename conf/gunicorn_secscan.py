a2013bc1b71fbba9679e6c39039fd4e59970948e
Kurtis Mullins <kmullins@redhat.com>
2020-06-03 15:31:00 -0400

    [PROJQUAY-480]  Add Prometheus Gauge for number of Gunicorn workers

--- conf/gunicorn_secscan.py
+++ conf/gunicorn_secscan.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 import logging
 
 from Crypto import Random
+from prometheus_client import Gauge
+
 from util.log import logfile_path
 from util.workers import get_worker_count, get_worker_connections_count
 
@@ -19,6 +21,13 @@ pythonpath = "."
 preload_app = True
 
 
+prometheus_workers_gauge = Gauge(
+    "quay_gunicorn_workers",
+    "number of gunicorn workers handling the security scanning endpoints",
+    labelnames=["gunicorn_workers"],
+)
+
+
 def post_fork(server, worker):
     # Reset the Random library to ensure it won't raise the "PID check failed." error after
     # gunicorn forks.
@@ -30,3 +39,14 @@ def when_ready(server):
     logger.debug(
         "Starting secscan gunicorn with %s workers and %s worker class", workers, worker_class
     )
+
+
+def nworkers_changed(server, new_value, old_value):
+    """
+    Called when the number of gunicorn workers is changed.
+    NOTE: old_value=None the first time this is called.
+    """
+    prometheus_workers_gauge.labels("secscan").set(new_value)
+    logger = logging.getLogger(__name__)
+    msg = "Changed gunicorn worker count from %s to %s" % (old_value, new_value)
+    logging.debug(msg)
--- conf/gunicorn_verbs.py
+++ conf/gunicorn_verbs.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 import logging
 
 from Crypto import Random
+from prometheus_client import Gauge
+
 from util.log import logfile_path
 from util.workers import get_worker_count, get_worker_connections_count
 
@@ -19,6 +21,13 @@ preload_app = True
 timeout = 2000  # Because sync workers
 
 
+prometheus_workers_gauge = Gauge(
+    "quay_gunicorn_workers",
+    "number of gunicorn workers handling the verb endpoints",
+    labelnames=["gunicorn_workers"],
+)
+
+
 def post_fork(server, worker):
     # Reset the Random library to ensure it won't raise the "PID check failed." error after
     # gunicorn forks.
@@ -28,3 +37,14 @@ def post_fork(server, worker):
 def when_ready(server):
     logger = logging.getLogger(__name__)
     logger.debug("Starting verbs gunicorn with %s workers and sync worker class", workers)
+
+
+def nworkers_changed(server, new_value, old_value):
+    """
+    Called when the number of gunicorn workers is changed.
+    NOTE: old_value=None the first time this is called.
+    """
+    prometheus_workers_gauge.labels("verbs").set(new_value)
+    logger = logging.getLogger(__name__)
+    msg = "Changed gunicorn worker count from %s to %s" % (old_value, new_value)
+    logging.debug(msg)
