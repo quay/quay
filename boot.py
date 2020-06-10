bf83ee9e69cf0d07b0670eb82d5ea22039ccdf03
Joseph Schorr <joseph.schorr@coreos.com>
2020-06-10 15:21:53 -0400

    Fix the initial expiration datetime in boot.py
    Fixes https://issues.redhat.com/browse/PROJQUAY-793

--- boot.py
+++ boot.py
@@ -85,7 +85,7 @@ def setup_jwt_proxy():
     else:
         # Generate the key for this Quay instance to use.
         minutes_until_expiration = app.config.get("INSTANCE_SERVICE_KEY_EXPIRATION", 120)
-        expiration = datetime.now() + timedelta(minutes=minutes_until_expiration)
+        expiration = datetime.utcnow() + timedelta(minutes=minutes_until_expiration)
         quay_key, quay_key_id = generate_key(
             app.config["INSTANCE_SERVICE_KEY_SERVICE"], get_audience(), expiration_date=expiration
         )
