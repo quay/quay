271dac55d6a3f44980b4060aa213f7c427fb6333
Joseph Schorr <joseph.schorr@coreos.com>
2020-06-15 15:35:06 -0400

    Fix formatting

--- image/oci/test/test_oci_index.py
+++ image/oci/test/test_oci_index.py
@@ -97,7 +97,7 @@ def test_index_without_amd():
     assert index.digest == "sha256:a0ed0f2b3949bc731063320667062307faf4245f6872dc5bc98ee6ea5443f169"
     assert index.local_blob_digests == []
     assert index.child_manifest_digests() == [
-        u"sha256:e692418e4cbaf90ca69d05a66403747baa33ee08806650b51fab815ad7fc331f",
-        u"sha256:5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270",
+        "sha256:e692418e4cbaf90ca69d05a66403747baa33ee08806650b51fab815ad7fc331f",
+        "sha256:5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270",
     ]
     assert index.amd64_linux_manifest_digest is None
