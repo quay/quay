c3c863f6767ae4e8aed4681ddfc1242d6044d488
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-08-31 19:19:35 +0530

    PROJQUAY-1020 chore: Fix cdnjs download failure(403) by setting custom User-Agent req header (#535)
    * fix: cdnjs download failure(403) by setting custom User-Agent req header
    
    * Update UA string
    
    * chore: Version lock black to 19.10b0

--- external_libraries.py
+++ external_libraries.py
@@ -111,6 +111,7 @@ def format_local_name(url):
 
 
 def _download_url(url):
+    req = urllib2.Request(url, headers={"User-Agent": "Quay (External Library Downloader)",})
     for index in range(0, MAX_RETRY_COUNT):
         try:
             response = urllib2.urlopen(url)
