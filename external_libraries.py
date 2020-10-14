ca9426a4da270e152cf358bfdc3943e18641e35a
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2020-10-14 15:41:16 -0400

    Fix merged cherrypicked commit from f9249d8b

--- external_libraries.py
+++ external_libraries.py
@@ -114,7 +114,7 @@ def _download_url(url):
     req = urllib2.Request(url, headers={"User-Agent": "Quay (External Library Downloader)",})
     for index in range(0, MAX_RETRY_COUNT):
         try:
-            response = urllib2.urlopen(url)
+            response = urllib2.urlopen(req)
             return response.read()
         except urllib2.URLError:
             logger.exception(
