e7505cbebfcde92829b4c26fb5ae41de64db91ed
Jonathan King <jonathankingfc@gmail.com>
2020-11-09 16:57:23 -0500

    fixed ldap validation

---
+++ pkg/lib/fieldgroups/ldap/ldap_test.go
@@ -0,0 +1,17 @@
+package ldap
+
+// // TestValidateLDAP tests the Validate function
+// func TestValidateLDAP(t testing.T) {
+// 	var tests = []struct {
+// 		name   string
+// 		config map[string]interface{}
+// 		want   string
+// 	}{{
+// 		config: map[string]interface{}{},
+// 		name:   "testOne",
+// 		want:   "valid",
+// 	}}
+// 	for _, tt := range tests {
+// 		fmt.Println("hello")
+// 	}
+// }
