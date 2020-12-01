f4b150dec9d8547a0750929ae49d445a58a35e77
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2020-12-01 13:41:24 -0500

    Allow basic auth on the secscan api endpoint when anonymous api resource is set.
    Allow the auth flow to use basic auth on the secscan api resource when
    FEATURE_ANONYMOUS_ACCESS (e.g CSO uses pull secrets to fetch secscan
    resources)

--- endpoints/api/test/test_secscan.py
+++ endpoints/api/test/test_secscan.py
@@ -1,6 +1,7 @@
 import base64
 
 import pytest
+from mock import patch
 
 from data.registry_model import registry_model
 from endpoints.test.shared import gen_basic_auth
@@ -11,25 +12,38 @@ from test.fixtures import *
 
 
 @pytest.mark.parametrize(
-    "endpoint",
+    "endpoint, anonymous_allowed, auth_headers, expected_code",
     [
-        RepositoryImageSecurity,
-        RepositoryManifestSecurity,
+        pytest.param(RepositoryImageSecurity, True, gen_basic_auth("devtable", "password"), 200),
+        pytest.param(RepositoryImageSecurity, False, gen_basic_auth("devtable", "password"), 200),
+        pytest.param(RepositoryManifestSecurity, True, gen_basic_auth("devtable", "password"), 200),
+        pytest.param(
+            RepositoryManifestSecurity, False, gen_basic_auth("devtable", "password"), 200
+        ),
+        pytest.param(RepositoryImageSecurity, True, None, 401),
+        pytest.param(RepositoryImageSecurity, False, None, 401),
+        pytest.param(RepositoryManifestSecurity, True, None, 401),
+        pytest.param(RepositoryManifestSecurity, False, None, 401),
     ],
 )
-def test_get_security_info_with_pull_secret(endpoint, client):
-    repository_ref = registry_model.lookup_repository("devtable", "simple")
-    tag = registry_model.get_repo_tag(repository_ref, "latest")
-    manifest = registry_model.get_manifest_for_tag(tag)
+def test_get_security_info_with_pull_secret(
+    endpoint, anonymous_allowed, auth_headers, expected_code, client
+):
+    with patch("features.ANONYMOUS_ACCESS", anonymous_allowed):
+        repository_ref = registry_model.lookup_repository("devtable", "simple")
+        tag = registry_model.get_repo_tag(repository_ref, "latest")
+        manifest = registry_model.get_manifest_for_tag(tag)
 
-    params = {
-        "repository": "devtable/simple",
-        "imageid": tag.manifest.legacy_image_root_id,
-        "manifestref": manifest.digest,
-    }
+        params = {
+            "repository": "devtable/simple",
+            "imageid": tag.manifest.legacy_image_root_id,
+            "manifestref": manifest.digest,
+        }
 
-    headers = {
-        "Authorization": gen_basic_auth("devtable", "password"),
-    }
+        headers = {}
+        if auth_headers is not None:
+            headers["Authorization"] = auth_headers
 
-    conduct_api_call(client, endpoint, "GET", params, None, headers=headers, expected_code=200)
+        conduct_api_call(
+            client, endpoint, "GET", params, None, headers=headers, expected_code=expected_code
+        )
