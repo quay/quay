e849607ef41db7b5ca87f85c8c0eeec79e076c45
Kenny Lee Sin Cheong <2530351+kleesc@users.noreply.github.com>
2021-07-08 17:13:39 -0400

    quayio: python2 syntax fix (#827)

--- endpoints/common.py
+++ endpoints/common.py
@@ -170,7 +170,7 @@ def render_page_template(name, route_data=None, **kwargs):
         current_year=datetime.datetime.now().year,
         kubernetes_namespace=IS_KUBERNETES and QE_NAMESPACE,
         account_recovery_mode=app.config.get("ACCOUNT_RECOVERY_MODE", False),
-        **kwargs,
+        **kwargs
     )
 
     resp = make_response(contents)
