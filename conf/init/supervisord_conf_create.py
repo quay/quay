7fe6d506a81e9d74a09586c165436ed7a3d736c4
Oleg Bulatov <obulatov@redhat.com>
2021-12-01 10:14:16 +0100

    Use /run for supervisord socket (#947)

--- conf/init/supervisord_conf_create.py
+++ conf/init/supervisord_conf_create.py
@@ -7,6 +7,7 @@ import jinja2
 QUAYPATH = os.getenv("QUAYPATH", ".")
 QUAYDIR = os.getenv("QUAYDIR", "/")
 QUAYCONF_DIR = os.getenv("QUAYCONF", os.path.join(QUAYDIR, QUAYPATH, "conf"))
+QUAYRUN_DIR = os.getenv("QUAYRUN", QUAYCONF_DIR)
 
 QUAY_LOGGING = os.getenv("QUAY_LOGGING", "stdout")  # or "syslog"
 
