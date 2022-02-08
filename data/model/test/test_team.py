562777c993539bdf846231b32be62f41b815ee43
Kenny Lee Sin Cheong <2530351+kleesc@users.noreply.github.com>
2022-02-08 10:52:46 -0500

    [redhat-3.6] teams: admin team deletion (PROJQUAY-2080)
    Cherrpick of #1095

--- data/model/test/test_team.py
+++ data/model/test/test_team.py
@@ -2,11 +2,16 @@ import pytest
 
 from data.model.team import (
     add_or_invite_to_team,
+    add_user_to_team,
     create_team,
     confirm_team_invite,
     list_team_users,
+    remove_team,
+    remove_user_from_team,
     validate_team_name,
+    __get_user_admin_teams,
 )
+from data.model import DataModelException
 from data.model.organization import create_organization
 from data.model.user import get_user, create_user_noverify
 
@@ -67,3 +72,46 @@ def test_invite_to_team(initialized_db):
 
     # Create another org and repeat, ensuring the same operations perform the same way.
     run_invite_flow("secondorg")
+
+
+def test_remove_team(initialized_db):
+    first_user = get_user("devtable")
+
+    # Create new org: devtable should be in the admin owners team
+    new_org = create_organization("testorg", "testorg" + "@example.com", first_user)
+    admin_teams = list(__get_user_admin_teams("testorg", "devtable"))
+
+    assert len(admin_teams) == 1 and admin_teams[0].name == "owners"
+
+    # Create new admin team without adding the devtable to the team:
+    # devtable should be able to delete the new admin team
+    new_team = create_team("testteam", new_org, "admin", description="test second admin team")
+    admin_teams = list(__get_user_admin_teams("testorg", "devtable"))
+    assert len(admin_teams) == 1 and admin_teams[0].name != "testteam"
+
+    # Removing the only team which devtable is the admin to should fail
+    with pytest.raises(DataModelException):
+        remove_team("testorg", "owners", "devtable")
+
+    # Removing the other admin team should succeed, since devtable is already admin in another team
+    remove_team("testorg", "testteam", "devtable")
+
+
+def test_remove_user_from_team(initialized_db):
+    first_user = get_user("devtable")
+    second_user = get_user("randomuser")
+
+    # Create new org: devtable should be in the admin owners team
+    new_org = create_organization("testorg", "testorg" + "@example.com", first_user)
+    admin_teams = list(__get_user_admin_teams("testorg", "devtable"))
+
+    # Add user to another admin team
+    new_team = create_team("testteam", new_org, "admin", description="test another admin team")
+    assert add_user_to_team(second_user, new_team)
+
+    # Cannot remove themselves from their only admin team
+    with pytest.raises(DataModelException):
+        remove_user_from_team("testorg", "testteam", "randomuser", "randomuser")
+
+    # Another admin should be able to
+    remove_user_from_team("testorg", "testteam", "randomuser", "devtable")
