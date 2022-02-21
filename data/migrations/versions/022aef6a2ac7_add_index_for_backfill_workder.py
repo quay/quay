3a8b03275e0287d485355e32f48dfe72b826f68c
Keith Westphal <kwestpha@redhat.com>
2022-02-21 15:02:55 -0600

    reset ranges and reformat

--- data/migrations/versions/022aef6a2ac7_add_index_for_backfill_workder.py
+++ data/migrations/versions/022aef6a2ac7_add_index_for_backfill_workder.py
@@ -7,14 +7,16 @@ Create Date: 2022-02-21 13:50:43.204422
 """
 
 # revision identifiers, used by Alembic.
-revision = '022aef6a2ac7'
-down_revision = '909d725887d3'
+revision = "022aef6a2ac7"
+down_revision = "909d725887d3"
 
 import sqlalchemy as sa
 
 
 def upgrade(op, tables, tester):
-    op.create_index("manifest_compressed_size_id", "manifest", ["layers_compressed_size", "id"], unique=False)
+    op.create_index(
+        "manifest_compressed_size_id", "manifest", ["layers_compressed_size", "id"], unique=False
+    )
 
 
 def downgrade(op, tables, tester):
