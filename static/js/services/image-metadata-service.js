99c28cb5c51031d6b040efcdfe7eea82cad0ce20
Syed <syed@apache.org>
2022-02-24 13:04:12 -0500

    ui: Depricate getImageCommand in security UI (PROJQUAY-3284)
    Image has been depricated in quay hence removing this from the UI as well

--- static/js/services/image-metadata-service.js
+++ static/js/services/image-metadata-service.js
@@ -4,29 +4,6 @@
 angular.module('quay').factory('ImageMetadataService', [function() {
   var metadataService = {};
 
-  metadataService.getImageCommand = function(image, imageId) {
-    if (!image) {
-      return null;
-    }
-
-    if (!image.__imageMap) {
-      image.__imageMap = {};
-      image.__imageMap[image.id] = image;
-
-      for (var i = 0; i < image.history.length; ++i) {
-        var cimage = image.history[i];
-        image.__imageMap[cimage.id] = cimage;
-      }
-    }
-
-    var found = image.__imageMap[imageId];
-    if (!found) {
-      return null;
-    }
-
-    return found.command;
-  };
-
   metadataService.getManifestCommand = function(manifest, blobDigest) {
     if (!manifest) {
       return null;
