a4ed9866089b43de9dbc3947fc5b340d5e71bc82
Flavian Missi <fmissi@redhat.com>
2022-04-12 16:04:01 +0200

    data: increase max len for proxy cache config credentials (#1241)
    fixes PROJQUAY-3521

---
+++ data/migrations/versions/b43b44996c26_increase_proxy_cache_config_creds_maxlen.py
@@ -0,0 +1,45 @@
+"""increase proxy cache config creds max len
+
+Revision ID: b43b44996c26
+Revises: 10d34e6536f8
+Create Date: 2022-04-12 09:14:00.910916
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "b43b44996c26"
+down_revision = "10d34e6536f8"
+
+import sqlalchemy as sa
+
+
+def upgrade(op, tables, tester):
+    op.alter_column(
+        "proxycacheconfig",
+        "upstream_registry_username",
+        type_=sa.String(length=4096),
+        nullable=True,
+    )
+
+    op.alter_column(
+        "proxycacheconfig",
+        "upstream_registry_password",
+        type_=sa.String(length=4096),
+        nullable=True,
+    )
+
+
+def downgrade(op, tables, tester):
+    op.alter_column(
+        "proxycacheconfig",
+        "upstream_registry_username",
+        type_=sa.String(length=2048),
+        nullable=True,
+    )
+
+    op.alter_column(
+        "proxycacheconfig",
+        "upstream_registry_password",
+        type_=sa.String(length=2048),
+        nullable=True,
+    )
