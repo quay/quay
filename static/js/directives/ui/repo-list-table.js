51c675139defa49c17c7847e89889af4d783cf4f
Sunandadadi <Sunandadadi@users.noreply.github.com>
2022-04-27 10:30:19 -0400

    Quay UI: Converting to nearest integer (PROJQUAY-3602) (#1285)
    * Quay UI: Rounding to 2 decimal places (PROJQUAY-3602)
    
    * Indentation fix
    
    * Converting to nearest integer

--- static/js/directives/ui/repo-list-table.js
+++ static/js/directives/ui/repo-list-table.js
@@ -30,7 +30,7 @@ angular.module('quay').directive('repoListTable', function () {
         'page': 0
       };
       $scope.disk_size_units = {
-	'KB': 1024,
+        'KB': 1024,
         'MB': 1024**2,
         'GB': 1024**3,
         'TB': 1024**4,
@@ -69,7 +69,7 @@ angular.module('quay').directive('repoListTable', function () {
         let byte_unit = null;
         for (const key in units) {
           byte_unit = units[key];
-          result = (bytes / $scope.disk_size_units[byte_unit]).toFixed(2);
+          result = Math.round(bytes / $scope.disk_size_units[byte_unit]);
           if (bytes >= $scope.disk_size_units[byte_unit]) {
             return result.toString() + " " + byte_unit;
           }
@@ -80,7 +80,7 @@ angular.module('quay').directive('repoListTable', function () {
 
       $scope.quotaPercentConsumed = function(repository) {
 	      if (repository.quota_report && repository.quota_report.configured_quota) {
-	        return (repository.quota_report.quota_bytes / repository.quota_report.configured_quota * 100).toFixed(2);
+	        return Math.round(repository.quota_report.quota_bytes / repository.quota_report.configured_quota * 100);
 	      }
 	      return 0;
       };
