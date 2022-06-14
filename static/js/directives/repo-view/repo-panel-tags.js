72b09f8365104cb4d66a8c700093a1769e3345df
Daniel Messer <dmesser@redhat.com>
2022-06-14 16:28:22 +0200

    sharpen regex for cosign manifest tag name convention
    Co-authored-by: Oleg Bulatov <oleg@bulatov.me>

--- static/js/directives/repo-view/repo-panel-tags.js
+++ static/js/directives/repo-view/repo-panel-tags.js
@@ -72,7 +72,7 @@ angular.module('quay').directive('repoPanelTags', function () {
       var matchCosignSignature = function(tag) {
         // matches cosign style tags and returns the match
 
-        var cosignNamingPattern = new RegExp('^sha256-([A-Fa-f0-9]{64})\.sig$');
+        var cosignNamingPattern = new RegExp('^sha256-[0-9a-f]{64}\.sig$');
         return tag.match(cosignNamingPattern);
       }
 
