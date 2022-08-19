fcc5528ee9e1e2eba26c8315011c68fd46b09dff
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2022-08-19 11:12:08 -0700

    init: ensure a newline is present before appending certs (PROJQUAY-3881) (#1487)
    Co-authored-by: Kenny Lee Sin Cheong <kenny.lee28@gmail.com>

--- conf/init/certs_install.sh
+++ conf/init/certs_install.sh
@@ -9,6 +9,13 @@ PYTHONUSERBASE_SITE_PACKAGE=${PYTHONUSERBASE_SITE_PACKAGE:-"$(python -m site --u
 
 cd ${QUAYDIR:-"/quay-registry"}
 
+function ensure_newline() {
+    lastline=$(tail -c 1 $1)
+    if [ "$lastline" != "" ]; then
+	echo >> "$1"
+    fi
+}
+
 # Add the custom LDAP certificate
 if [ -e $QUAYCONFIG/ldap.crt ]
 then
@@ -20,6 +27,13 @@ if [ -d $CERTDIR ]; then
   if test "$(ls -A "$CERTDIR")"; then
       echo "Installing extra certificates found in $CERTDIR directory"
       cp $CERTDIR/* ${SYSTEM_CERTDIR}
+
+      CERT_FILES="$CERTDIR/*"
+      for f in $CERT_FILES
+      do
+	  ensure_newline "$f"
+      done
+
       cat $CERTDIR/* >> $PYTHONUSERBASE_SITE_PACKAGE/certifi/cacert.pem
   fi
 fi
@@ -28,6 +42,7 @@ fi
 if [ -f $CERTDIR ]; then
   echo "Installing extra certificates found in $CERTDIR file"
   csplit -z -f ${SYSTEM_CERTDIR}/extra-ca- $CERTDIR  '/-----BEGIN CERTIFICATE-----/' '{*}'
+  ensure_newline "$CERTDIR"
   cat $CERTDIR >> $PYTHONUSERBASE_SITE_PACKAGE/certifi/cacert.pem
 fi
 
@@ -36,6 +51,7 @@ for f in $(find -L $QUAYCONFIG/ -maxdepth 1 -type f -name "extra_ca*")
 do
  echo "Installing extra cert $f"
  cp "$f" ${SYSTEM_CERTDIR}
+ ensure_newline "$f"
  cat "$f" >> $PYTHONUSERBASE_SITE_PACKAGE/certifi/cacert.pem
 done
 
