68f0017e013da4f2c8c9ee9869d6d97763f40b95
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2022-12-05 16:53:10 -0500

    Update nginx config (#1639)
    Signed-off-by: Dave O'Connor <doconnor@redhat.com>

---
+++ test/integration/test_nginx.py
@@ -0,0 +1,19 @@
+import requests
+import pytest
+
+host = "http://localhost:8080"
+
+
+@pytest.mark.parametrize(
+    "path, status",
+    [
+        ("/repository/", 200),
+        ("/repository/projectquay/clair-jwt", 200),
+        ("/organization/projectquay/", 200),
+        ("/user/user1/?tab=settings", 200),
+        ("/search?q=", 200),
+    ],
+)
+def test_nginx_ok(path, status):
+    r = requests.head(f"{host}{path}", allow_redirects=False)
+    assert r.status_code == status
