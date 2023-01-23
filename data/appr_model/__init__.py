4ced683c2b1f0a79bd208e5d0f1088bc742348a7
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2023-01-23 13:32:41 -0500

    chore: remove deprecated appr code (PROJQUAY-4992)

--- data/appr_model/__init__.py
+++
@@ -1,9 +0,0 @@
-from data.appr_model import (
-    blob,
-    channel,
-    manifest,
-    manifest_list,
-    package,
-    release,
-    tag,
-)
--- initdb.py
+++ initdb.py
@@ -492,15 +492,6 @@ def initialize_database():
     MediaType.create(name="text/plain")
     MediaType.create(name="application/json")
     MediaType.create(name="text/markdown")
-    MediaType.create(name="application/vnd.cnr.blob.v0.tar+gzip")
-    MediaType.create(name="application/vnd.cnr.package-manifest.helm.v0.json")
-    MediaType.create(name="application/vnd.cnr.package-manifest.kpm.v0.json")
-    MediaType.create(name="application/vnd.cnr.package-manifest.docker-compose.v0.json")
-    MediaType.create(name="application/vnd.cnr.package.kpm.v0.tar+gzip")
-    MediaType.create(name="application/vnd.cnr.package.helm.v0.tar+gzip")
-    MediaType.create(name="application/vnd.cnr.package.docker-compose.v0.tar+gzip")
-    MediaType.create(name="application/vnd.cnr.manifests.v0.json")
-    MediaType.create(name="application/vnd.cnr.manifest.list.v0.json")
 
     for media_type in DOCKER_SCHEMA1_CONTENT_TYPES:
         MediaType.create(name=media_type)
