4d2745becc39386de3689167ebaf5876114cedd4
dmesser <dmesser@redhat.com>
2023-05-12 22:32:47 +0200

    move alembic patch to head
    Signed-off-by: dmesser <dmesser@redhat.com>

--- data/migrations/versions/0246c2d0e750_add_login_logout_logentrykind.py
+++ data/migrations/versions/0246c2d0e750_add_login_logout_logentrykind.py
@@ -1,14 +1,14 @@
 """add login logout logentrykind
 
 Revision ID: 0246c2d0e750
-Revises: a648ab200ab7
+Revises: 43fea47d641f
 Create Date: 2023-05-4 09:44:57.391686
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "0246c2d0e750"
-down_revision = "a648ab200ab7"
+down_revision = "43fea47d641f"
 
 import sqlalchemy as sa
 
