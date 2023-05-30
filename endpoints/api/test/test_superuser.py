540cf0f88997e81919b4438c426c0e64fd2c71be
Brandon Caton <bcaton@redhat.com>
2023-05-30 09:27:48 -0400

    superuser: paginate user's list (PROJQUAY-4297) (#1881) (#1909)
    Allow loading of superuser user list for larger registries by paginating API call.

--- endpoints/api/test/test_superuser.py
+++ endpoints/api/test/test_superuser.py
@@ -1,4 +1,5 @@
 import pytest
+from data.database import DeletedNamespace, User
 
 from endpoints.api.superuser import SuperUserList, SuperUserManagement, SuperUserOrganizationList
 from endpoints.api.test.shared import conduct_api_call
@@ -40,6 +41,18 @@ def test_paginate_orgs(client):
             cl, SuperUserOrganizationList, "GET", params, None, 200
         ).json
         assert len(secondResult["organizations"]) == 2
+        assert secondResult.get("next_page", None) is None
+
+
+def test_paginate_test_list_all_users(client):
+    with client_with_identity("devtable", client) as cl:
+        params = {"limit": 6}
+        firstResult = conduct_api_call(cl, SuperUserList, "GET", params, None, 200).json
+        assert len(firstResult["users"]) == 6
+        assert firstResult["next_page"] is not None
+        params["next_page"] = firstResult["next_page"]
+        secondResult = conduct_api_call(cl, SuperUserList, "GET", params, None, 200).json
+        assert len(secondResult["users"]) == 4
         assert secondResult.get("next_page", None) is None
 
 
