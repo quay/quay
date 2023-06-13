994418b66995666659fd6cfda118fbc5208e0f47
bcaton <bcaton@redhat.com>
2023-06-13 11:32:22 -0400

    updating sizes from base 1000 to 1024

--- static/js/directives/filters/bytes.js
+++ static/js/directives/filters/bytes.js
@@ -5,8 +5,8 @@ angular.module('quay').filter('bytes', function() {
   return function(bytes, precision) {
     if (!bytes || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return 'Unknown';
     if (typeof precision === 'undefined') precision = 1;
-    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-    number = Math.floor(Math.log(bytes) / Math.log(1000));
-    return (bytes / Math.pow(1000, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    var units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB'],
+    number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
   }
 });
--- static/js/directives/ui/quota-management-view.js
+++ static/js/directives/ui/quota-management-view.js
@@ -25,10 +25,10 @@ angular.module('quay').directive('quotaManagementView', function () {
 
       $scope.defer = null;
       $scope.disk_size_units = {
-        'KB': 1000,
-        'MB': 1000 ** 2,
-        'GB': 1000 ** 3,
-        'TB': 1000 ** 4,
+        'KiB': 1024,
+        'MiB': 1024 ** 2,
+        'GiB': 1024 ** 3,
+        'TiB': 1024 ** 4,
       };
       $scope.quotaUnits = Object.keys($scope.disk_size_units);
       $scope.rejectLimitType = 'Reject';
