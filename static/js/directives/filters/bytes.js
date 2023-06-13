ae25e497618a7d67e4a8a79db4fa2eb35d60b241
bcaton <bcaton@redhat.com>
2023-06-13 09:52:58 -0400

    displaying tag sizes as MB instead of MiB

--- static/js/directives/filters/bytes.js
+++ static/js/directives/filters/bytes.js
@@ -6,7 +6,7 @@ angular.module('quay').filter('bytes', function() {
     if (!bytes || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return 'Unknown';
     if (typeof precision === 'undefined') precision = 1;
     var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-    number = Math.floor(Math.log(bytes) / Math.log(1024));
-    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    number = Math.floor(Math.log(bytes) / Math.log(1000));
+    return (bytes / Math.pow(1000, Math.floor(number))).toFixed(precision) + ' ' + units[number];
   }
 });
