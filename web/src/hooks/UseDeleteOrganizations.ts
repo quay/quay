3c878c69f34d99408aaa53dbf792fbfc76830694
Sunandadadi <sunanda.3094@gmail.com>
2023-06-27 10:16:24 -0400

    UI: Delete normal user org by super user (PROJQUAY-5639)

--- web/src/hooks/UseDeleteOrganizations.ts
+++ web/src/hooks/UseDeleteOrganizations.ts
@@ -1,12 +1,20 @@
 import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {bulkDeleteOrganizations} from 'src/resources/OrganizationResource';
+import {useQuayConfig} from './UseQuayConfig';
+import {useEffect, useState} from "react";
 
 export function useDeleteOrganizations({onSuccess, onError}) {
   const queryClient = useQueryClient();
+  const quayConfig = useQuayConfig();
+  const [isSuperUser, setSuperUser] = useState(false);
+
+  useEffect(() => {
+    setSuperUser(quayConfig?.features?.SUPERUSERS_FULL_ACCESS);
+  })
 
   const deleteOrganizationsMutator = useMutation(
     async (orgs: string[]) => {
-      await bulkDeleteOrganizations(orgs);
+      await bulkDeleteOrganizations(orgs, isSuperUser);
     },
     {
       onSuccess: () => {
