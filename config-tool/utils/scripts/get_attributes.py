17c94959521e0ec347530228aaedec4395ff5f34
Oleg Bulatov <oleg@bulatov.me>
2023-07-31 17:32:14 +0200

    chore: Reformat python scripts in config-tool

--- config-tool/utils/scripts/get_attributes.py
+++ config-tool/utils/scripts/get_attributes.py
@@ -5,11 +5,11 @@ with open("/home/jonathan/Desktop/config-tool/pkg/lib/testdata/quay-config-schem
 
     attributes = []
 
-    for prop_name in config['properties'].keys():
+    for prop_name in config["properties"].keys():
         attributes.append(prop_name)
 
-    with open("/home/jonathan/Desktop/config-tool/attributes.txt","w") as outfile:
+    with open("/home/jonathan/Desktop/config-tool/attributes.txt", "w") as outfile:
 
         for prop_name in attributes:
 
-            outfile.write(prop_name+"\n")
+            outfile.write(prop_name + "\n")
