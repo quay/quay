f3b10ad7c4501e4ee73c7cac18f1a3833cf76433
Michaela Lang <milang@redhat.com>
2023-08-30 15:46:39 +0200

    fixed error message to proper english and added schema definition for the option

--- data/model/user.py
+++ data/model/user.py
@@ -325,7 +325,7 @@ def update_enabled(user, set_enabled):
 def create_robot(robot_shortname, parent, description="", unstructured_metadata=None, token=None):
     (username_valid, username_issue) = validate_username(robot_shortname)
     if config.app_config.get("ROBOTS_DISALLOW", False):
-        msg = "Robot account has been disabled. Please contact your administrator."
+        msg = "Robot accounts have beeen disabled. Please contact your administrator."
         raise InvalidRobotException(msg)
 
     if not username_valid:
@@ -440,7 +440,7 @@ def get_matching_robots(name_prefix, username, limit=10):
 
 def verify_robot(robot_username, password):
     if config.app_config.get("ROBOTS_DISALLOW", False):
-        msg = "Robot account has been disabled. Please contact your administrator."
+        msg = "Robot accounts have been disabled. Please contact your administrator."
         raise InvalidRobotException(msg)
     try:
         password.encode("ascii")
