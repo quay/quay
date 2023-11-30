f01a2a93a39f93b79cd216053a365eb4a2597aaf
Sunandadadi <sunanda.3094@gmail.com>
2023-11-29 23:13:31 -0500

    adding model_cache parameter to lookup_repository func for interface and registry_proxy_model

--- data/registry_model/interface.py
+++ data/registry_model/interface.py
@@ -43,7 +43,13 @@ class RegistryDataInterface(object):
 
     @abstractmethod
     def lookup_repository(
-        self, namespace_name, repo_name, kind_filter=None, raise_on_error=False, manifest_ref=None
+        self,
+        namespace_name,
+        repo_name,
+        kind_filter=None,
+        raise_on_error=False,
+        manifest_ref=None,
+        model_cache=None,
     ):
         """
         Looks up and returns a reference to the repository with the given namespace and name, or
