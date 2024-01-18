6c8393836d4f4a8d748d0011ea97d014150c85bd
Oleg Bulatov <oleg@bulatov.me>
2024-01-18 16:04:03 +0100

    chore: use oci_model directly without proxy object

--- data/registry_model/__init__.py
+++ data/registry_model/__init__.py
@@ -1,23 +1,3 @@
-import logging
-import os
-
 from data.registry_model.registry_oci_model import oci_model
 
-logger = logging.getLogger(__name__)
-
-
-class RegistryModelProxy(object):
-    def __init__(self):
-        self._model = oci_model
-
-    def __getattr__(self, attr):
-        return getattr(self._model, attr)
-
-    def set_id_hash_salt(self, hash_salt):
-        self._model.set_id_hash_salt(hash_salt)
-
-
-registry_model = RegistryModelProxy()
-logger.info("===============================")
-logger.info("Using registry model `%s`", registry_model._model)
-logger.info("===============================")
+registry_model = oci_model
