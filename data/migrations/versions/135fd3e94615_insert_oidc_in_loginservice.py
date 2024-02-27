fbfc0b63639b7b52a039a7c8a9a9fa586dbf4480
Sunandadadi <sunanda.3094@gmail.com>
2024-02-27 16:16:36 -0500

    update down version for alembic migration

--- data/migrations/versions/135fd3e94615_insert_oidc_in_loginservice.py
+++ data/migrations/versions/135fd3e94615_insert_oidc_in_loginservice.py
@@ -1,14 +1,14 @@
 """insert oidc in loginservice
 
 Revision ID: 135fd3e94615
-Revises: 41d15c93c299
+Revises: b4da5b09c8df
 Create Date: 2024-02-21 09:48:54.463860
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "135fd3e94615"
-down_revision = "41d15c93c299"
+down_revision = "b4da5b09c8df"
 
 import sqlalchemy as sa
 
