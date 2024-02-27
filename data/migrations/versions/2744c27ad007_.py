403abeb4faffef96fe4f8f92386faced053400eb
Sunandadadi <sunanda.3094@gmail.com>
2024-02-27 13:52:58 -0500

    merging alembic heads

---
+++ data/migrations/versions/2744c27ad007_.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: 2744c27ad007
+Revises: ('b4da5b09c8df', '135fd3e94615')
+Create Date: 2024-02-27 18:49:46.761649
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "2744c27ad007"
+down_revision = ("b4da5b09c8df", "135fd3e94615")
+
+import sqlalchemy as sa
+
+
+def upgrade(op, tables, tester):
+    pass
+
+
+def downgrade(op, tables, tester):
+    pass
