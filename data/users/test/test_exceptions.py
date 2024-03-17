bc4e6b1c52ecf5f8c24d69a381c37aa724aff05c
Michaela Lang <milang@redhat.com>
2024-03-17 15:13:15 +0100

    added unittests for exceptions introduced in PR-2761

---
+++ data/users/test/test_exceptions.py
@@ -0,0 +1,229 @@
+import logging
+
+import ldap
+
+from data.users.externalldap import LDAPUsers, logger
+
+
+class ExceptionLogHandler(logging.StreamHandler):
+    on_same_line = False
+    content = []
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            self.content.append(msg)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+
+exceptHandler = ExceptionLogHandler()
+logger.addHandler(exceptHandler)
+
+
+def test_ldap_user_search():
+    # reset content from log
+    exceptHandler.content = []
+    user = LDAPUsers(
+        ldap_uri="invalid",
+        base_dn=["dc=example", "dc=com"],
+        admin_dn="cn=invalid,dc=example,dc=com",
+        admin_passwd="stillinvalid",
+        user_rdn="ou=people,dc=example,dc=com",
+        uid_attr="uid",
+        email_attr="mail",
+        timeout=1,
+        network_timeout=1,
+    )
+
+    def raiseException(ex):
+        raise ex(*[{"info": "Can't contact LDAP server", "desc": "Can't contact LDAP server"}])
+
+    user._ldap.get_connection = lambda: raiseException(ldap.SERVER_DOWN)
+    assert user._ldap_user_search(None) == (None, "Empty username/email")
+    assert user._ldap_user_search("someone") == (None, "Invalid username or password.")
+    assert exceptHandler.content[0] == "SERVER_DOWN -1 Can't contact LDAP server"
+    # reset content from log
+    exceptHandler.content = []
+
+
+def test_ldap_warning_exceptions_user_search():
+    # reset content from log
+    exceptHandler.content = []
+    for exception in (
+        (ldap.AUTH_UNKNOWN, -6),
+        (ldap.CONFIDENTIALITY_REQUIRED, 13),
+        (ldap.CONSTRAINT_VIOLATION, 19),
+        (ldap.INVALID_DN_SYNTAX, 34),
+        (ldap.INVALID_SYNTAX, 21),
+        (ldap.SERVER_DOWN, -1),
+        (ldap.STRONG_AUTH_NOT_SUPPORTED, 7),
+        (ldap.STRONG_AUTH_REQUIRED, 8),
+        (ldap.UNAVAILABLE, 52),
+        (ldap.UNWILLING_TO_PERFORM, 53),
+        (ldap.CONTROL_NOT_FOUND, -13),
+        (ldap.INAPPROPRIATE_AUTH, 48),
+        (ldap.INSUFFICIENT_ACCESS, 50),
+    ):
+        user = LDAPUsers(
+            ldap_uri="ldaps://localhost",
+            base_dn=["dc=example", "dc=com"],
+            admin_dn="cn=invalid,dc=example,dc=com",
+            admin_passwd="stillinvalid",
+            user_rdn="ou=people,dc=example,dc=com",
+            uid_attr="uid",
+            email_attr="mail",
+            timeout=1,
+            network_timeout=1,
+        )
+
+        def raiseException(ex):
+            raise ex(*[{"info": "test", "desc": "test"}])
+
+        user._ldap.get_connection = lambda: raiseException(exception[0])
+        assert user._ldap_user_search("someone") == (None, "Invalid username or password.")
+        assert exceptHandler.content[0] == f"{exception[0].__name__} {exception[1]} test"
+        # reset content from log
+        exceptHandler.content = []
+
+
+def test_ldap_warning_exceptions_ping():
+    # reset content from log
+    exceptHandler.content = []
+    for exception in (
+        (ldap.AUTH_UNKNOWN, -6),
+        (ldap.CONFIDENTIALITY_REQUIRED, 13),
+        (ldap.CONSTRAINT_VIOLATION, 19),
+        (ldap.INVALID_DN_SYNTAX, 34),
+        (ldap.INVALID_SYNTAX, 21),
+        (ldap.SERVER_DOWN, -1),
+        (ldap.STRONG_AUTH_NOT_SUPPORTED, 7),
+        (ldap.STRONG_AUTH_REQUIRED, 8),
+        (ldap.UNAVAILABLE, 52),
+        (ldap.UNWILLING_TO_PERFORM, 53),
+        (ldap.CONTROL_NOT_FOUND, -13),
+        (ldap.INAPPROPRIATE_AUTH, 48),
+        (ldap.INSUFFICIENT_ACCESS, 50),
+    ):
+        user = LDAPUsers(
+            ldap_uri="ldaps://localhost",
+            base_dn=["dc=example", "dc=com"],
+            admin_dn="cn=invalid,dc=example,dc=com",
+            admin_passwd="stillinvalid",
+            user_rdn="ou=people,dc=example,dc=com",
+            uid_attr="uid",
+            email_attr="mail",
+            timeout=1,
+            network_timeout=1,
+        )
+
+        def raiseException(ex):
+            raise ex(*[{"info": "test", "desc": "test"}])
+
+        user._ldap.get_connection = lambda: raiseException(exception[0])
+        assert user.ping() == (False, "test")
+        assert exceptHandler.content[0] == f"{exception[0].__name__} {exception[1]} test"
+        # reset content from log
+        exceptHandler.content = []
+
+
+def test_ldap_warning_exception_at_least_one_user_exists():
+    # reset content from log
+    exceptHandler.content = []
+    for exception in (
+        (ldap.AUTH_UNKNOWN, -6),
+        (ldap.CONFIDENTIALITY_REQUIRED, 13),
+        (ldap.CONSTRAINT_VIOLATION, 19),
+        (ldap.INVALID_DN_SYNTAX, 34),
+        (ldap.INVALID_SYNTAX, 21),
+        (ldap.SERVER_DOWN, -1),
+        (ldap.STRONG_AUTH_NOT_SUPPORTED, 7),
+        (ldap.STRONG_AUTH_REQUIRED, 8),
+        (ldap.UNAVAILABLE, 52),
+        (ldap.UNWILLING_TO_PERFORM, 53),
+        (ldap.CONTROL_NOT_FOUND, -13),
+        (ldap.INAPPROPRIATE_AUTH, 48),
+        (ldap.INSUFFICIENT_ACCESS, 50),
+    ):
+        user = LDAPUsers(
+            ldap_uri="ldaps://localhost",
+            base_dn=["dc=example", "dc=com"],
+            admin_dn="cn=invalid,dc=example,dc=com",
+            admin_passwd="stillinvalid",
+            user_rdn="ou=people,dc=example,dc=com",
+            uid_attr="uid",
+            email_attr="mail",
+            timeout=1,
+            network_timeout=1,
+        )
+
+        def raiseException(ex):
+            raise ex(*[{"info": "test", "desc": "test"}])
+
+        user._ldap.get_connection = lambda: raiseException(exception[0])
+        assert user.at_least_one_user_exists() == (None, "LDAP Admin dn or password is invalid")
+        assert exceptHandler.content[0] == f"{exception[0].__name__} {exception[1]} test"
+        # reset content from log
+        exceptHandler.content = []
+
+
+def test_ldap_warning_exception_verify_credentials():
+    # reset content from log
+    exceptHandler.content = []
+    for exception in (
+        (ldap.AUTH_UNKNOWN, -6),
+        (ldap.CONFIDENTIALITY_REQUIRED, 13),
+        (ldap.CONSTRAINT_VIOLATION, 19),
+        (ldap.INVALID_DN_SYNTAX, 34),
+        (ldap.INVALID_SYNTAX, 21),
+        (ldap.SERVER_DOWN, -1),
+        (ldap.STRONG_AUTH_NOT_SUPPORTED, 7),
+        (ldap.STRONG_AUTH_REQUIRED, 8),
+        (ldap.UNAVAILABLE, 52),
+        (ldap.UNWILLING_TO_PERFORM, 53),
+        (ldap.CONTROL_NOT_FOUND, -13),
+        (ldap.INAPPROPRIATE_AUTH, 48),
+        (ldap.INSUFFICIENT_ACCESS, 50),
+    ):
+        user = LDAPUsers(
+            ldap_uri="ldaps://localhost",
+            base_dn=["dc=example", "dc=com"],
+            admin_dn="cn=invalid,dc=example,dc=com",
+            admin_passwd="stillinvalid",
+            user_rdn="ou=people,dc=example,dc=com",
+            uid_attr="uid",
+            email_attr="mail",
+            timeout=1,
+            network_timeout=1,
+        )
+
+        def raiseException(ex):
+            raise ex(*[{"info": "test", "desc": "test"}])
+
+        user._ldap.get_connection = lambda: raiseException(exception[0])
+        assert user.verify_credentials("someone", "changeme") == (
+            None,
+            "Invalid username or password.",
+        )
+        assert exceptHandler.content[0] == f"{exception[0].__name__} {exception[1]} test"
+        # reset content from log
+        exceptHandler.content = []
+
+
+def test_ldap_catchall_exception_verify_credentials():
+    # reset content from log
+    exceptHandler.content = []
+
+    def raiseException(ex):
+        raise ex("debuglog level only")
+
+    user._ldap.get_connection = lambda: raiseException(IOError)
+    assert user.verify_credentials("someone", "changeme") == (None, "Invalid username or password.")
+    assert exceptHandler.content != []
+    logger.setLevel(logging.DEBUG)
+    assert user.verify_credentials("someone", "changeme") == (None, "Invalid username or password.")
+    assert exceptHandler.content[0] == "debuglog level only"
+    # reset content from log
+    exceptHandler.content = []
