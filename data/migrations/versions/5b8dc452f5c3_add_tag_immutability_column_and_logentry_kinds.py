f4164bd7b3759769e67e2851aba16b4a143095cc
dmesser <dmesser@redhat.com>
2024-03-24 02:05:09 +0100

    bump alembic head
    Signed-off-by: dmesser <dmesser@redhat.com>

--- data/migrations/versions/5b8dc452f5c3_add_tag_immutability_column_and_logentry_kinds.py
+++ data/migrations/versions/5b8dc452f5c3_add_tag_immutability_column_and_logentry_kinds.py
@@ -1,14 +1,14 @@
 """add tag immutability column
 
 Revision ID: 5b8dc452f5c3
-Revises: 41d15c93c299
+Revises: 36cd2b747a08
 Create Date: 2023-05-21 13:13:10.565161
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "5b8dc452f5c3"
-down_revision = "41d15c93c299"
+down_revision = "36cd2b747a08"
 
 import sqlalchemy as sa
 
