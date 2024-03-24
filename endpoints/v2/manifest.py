c69771ad45b17056d4cd834c54f9108750898ab5
dmesser <dmesser@redhat.com>
2024-03-24 11:53:31 +0100

    formatting
    Signed-off-by: dmesser <dmesser@redhat.com>

--- endpoints/v2/manifest.py
+++ endpoints/v2/manifest.py
@@ -398,7 +398,9 @@ def delete_manifest_by_digest(namespace_name, repo_name, manifest_ref):
             raise ManifestUnknown()
 
         try:
-            tags = registry_model.delete_tags_for_manifest(model_cache, manifest, raise_on_error=True)
+            tags = registry_model.delete_tags_for_manifest(
+                model_cache, manifest, raise_on_error=True
+            )
         except TagImmutableException as e:
             raise TagImmutable(message=str(e))
 
