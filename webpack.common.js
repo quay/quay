baa84e9a995417fc48b1bb0590625f66a551b79e
Syed <syed@apache.org>
2024-04-27 10:58:18 -0400

    add eslintrc and fix tsconfig.json

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const BG_IMAGES_DIRNAME = 'assets';
-const ASSET_PATH = process.env.ASSET_PATH || '/';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const BG_IMAGES_DIRNAME = "assets";
+const ASSET_PATH = process.env.ASSET_PATH || "/";
 module.exports = (env) => {
   return {
     entry: "./web/src/index.tsx",
@@ -13,7 +13,7 @@ module.exports = (env) => {
           test: /\.(tsx|ts|jsx)?$/,
           use: [
             {
-              loader: 'ts-loader',
+              loader: "ts-loader",
               options: {
                 transpileOnly: true,
                 experimentalWatchApi: true,
@@ -26,38 +26,38 @@ module.exports = (env) => {
           // only process modules with this loader
           // if they live under a 'fonts' or 'pficon' directory
           include: [
-            path.resolve(__dirname, 'node_modules/patternfly/dist/fonts'),
+            path.resolve(__dirname, "node_modules/patternfly/dist/fonts"),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-core/dist/styles/assets/fonts',
+              "node_modules/@patternfly/react-core/dist/styles/assets/fonts",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-core/dist/styles/assets/pficon',
+              "node_modules/@patternfly/react-core/dist/styles/assets/pficon",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/patternfly/assets/fonts',
+              "node_modules/@patternfly/patternfly/assets/fonts",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/patternfly/assets/pficon',
+              "node_modules/@patternfly/patternfly/assets/pficon",
             ),
           ],
           generator: {
-            filename: 'assets/[name].[ext]',
+            filename: "assets/[name].[ext]",
           },
         },
         {
           test: /\.svg$/,
-          include: (input) => input.indexOf('background-filter.svg') > 1,
+          include: (input) => input.indexOf("background-filter.svg") > 1,
           use: [
             {
-              loader: 'url-loader',
+              loader: "url-loader",
               options: {
                 limit: 5000,
-                outputPath: 'svgs',
-                name: '[name].[ext]',
+                outputPath: "svgs",
+                name: "[name].[ext]",
               },
             },
           ],
@@ -68,7 +68,7 @@ module.exports = (env) => {
           // this is primarily useful when applying a CSS background using an SVG
           include: (input) => input.indexOf(BG_IMAGES_DIRNAME) > -1,
           use: {
-            loader: 'svg-url-loader',
+            loader: "svg-url-loader",
             options: {},
           },
         },
@@ -78,50 +78,50 @@ module.exports = (env) => {
           // 'fonts', or 'pficon' directory, those are handled with other loaders
           include: (input) =>
             input.indexOf(BG_IMAGES_DIRNAME) === -1 &&
-            input.indexOf('fonts') === -1 &&
-            input.indexOf('background-filter') === -1 &&
-            input.indexOf('pficon') === -1,
+            input.indexOf("fonts") === -1 &&
+            input.indexOf("background-filter") === -1 &&
+            input.indexOf("pficon") === -1,
           use: {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {},
           },
         },
         {
           test: /\.(jpg|jpeg|png|gif)$/i,
           include: [
-            path.resolve(__dirname, 'node_modules/patternfly'),
+            path.resolve(__dirname, "node_modules/patternfly"),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/patternfly/assets/images',
+              "node_modules/@patternfly/patternfly/assets/images",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-styles/css/assets/images',
+              "node_modules/@patternfly/react-styles/css/assets/images",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-core/dist/styles/assets/images',
+              "node_modules/@patternfly/react-core/dist/styles/assets/images",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images',
+              "node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images',
+              "node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images",
             ),
             path.resolve(
               __dirname,
-              'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css/assets/images',
+              "node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css/assets/images",
             ),
           ],
           use: [
             {
-              loader: 'url-loader',
+              loader: "url-loader",
               options: {
                 limit: 5000,
-                outputPath: 'images',
-                name: '[name].[ext]',
+                outputPath: "images",
+                name: "[name].[ext]",
               },
             },
           ],
@@ -130,34 +130,34 @@ module.exports = (env) => {
           test: /\.s[ac]ss$/i,
           use: [
             // Creates `style` nodes from JS strings
-            'style-loader',
+            "style-loader",
             // Translates CSS into CommonJS
-            'css-loader',
+            "css-loader",
             // Compiles Sass to CSS
-            'sass-loader',
+            "sass-loader",
           ],
         },
       ],
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
       publicPath: ASSET_PATH,
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'web', 'src', 'index.html'),
+        template: path.resolve(__dirname, "web", "src", "index.html"),
       }),
       new CopyPlugin({
-        patterns: [{from: './web/src/assets/favicon.png', to: 'images'}],
+        patterns: [{ from: "./web/src/assets/favicon.png", to: "images" }],
       }),
     ],
     resolve: {
       alias: { src: "web/src" },
-      extensions: ['.js', '.ts', '.tsx', '.jsx'],
+      extensions: [".js", ".ts", ".tsx", ".jsx"],
       plugins: [
         new TsconfigPathsPlugin({
-          configFile: path.resolve(__dirname, './tsconfig.json'),
+          configFile: path.resolve(__dirname, "./tsconfig.json"),
         }),
       ],
       symlinks: false,
