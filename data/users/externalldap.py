b36918760186b0b4b4bc53a1cc66c78df32d82ce
Michaela Lang <milang@redhat.com>
2024-06-24 11:26:08 +0200

    bumped PROJQUAY-5196 to latest master
    Signed-off-by: Michaela Lang <milang@redhat.com>

--- data/users/externalldap.py
+++ data/users/externalldap.py
@@ -567,6 +567,10 @@ class LDAPUsers(FederatedUsers):
         return True
 
     def is_restricted_user(self, username_or_email: str) -> bool:
+        # do not restrict superusers
+        if self.is_superuser(username_or_email):
+            return False
+
         if not username_or_email:
             return False
 
