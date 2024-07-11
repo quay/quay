a0dabcb542e6a2e9c20381bf8c78cb21886a9fe5
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2024-07-11 16:41:58 +0200

    [redhat-3.12] ui: add helper text for input (PROJQUAY-7442) (#3017)
    * ui: add helper text for input (PROJQUAY-7442)
    
    * minor fixes
    
    * fixing tests
    
    * minor revert
    
    ---------
    
    Co-authored-by: Sunandadadi <sunanda.3094@gmail.com>

--- web/src/routes/RepositoryDetails/Settings/RepoEventExpiry.tsx
+++ web/src/routes/RepositoryDetails/Settings/RepoEventExpiry.tsx
@@ -1,9 +1,18 @@
-import {FormGroup, TextInput, ValidatedOptions} from '@patternfly/react-core';
+import {
+  FormGroup,
+  TextInput,
+  ValidatedOptions,
+  HelperText,
+  HelperTextItem,
+} from '@patternfly/react-core';
+import Conditional from 'src/components/empty/Conditional';
 import {NotificationEventConfig} from 'src/hooks/UseEvents';
 import {useState} from 'react';
 
 export default function RepoEventExpiry(props: RepoEventExpiryProps) {
-  const [valid, setValid] = useState(ValidatedOptions.default);
+  const [valid, setValid] = useState<ValidatedOptions>(
+    ValidatedOptions.default,
+  );
 
   const onChange = (_event, value) => {
     props.setEventConfig({days: Number(value)});
@@ -21,13 +30,20 @@ export default function RepoEventExpiry(props: RepoEventExpiryProps) {
       isRequired
     >
       <TextInput
-        value={props.eventConfig?.days}
+        value={props.eventConfig?.days || ''}
         onChange={onChange}
         type={'number'}
         id="days-to-image-expiry"
         required
         validated={valid}
       />
+      <Conditional if={valid == ValidatedOptions.error}>
+        <HelperText>
+          <HelperTextItem variant="error" hasIcon>
+            number of days should be more than 0.
+          </HelperTextItem>
+        </HelperText>
+      </Conditional>
     </FormGroup>
   );
 }
