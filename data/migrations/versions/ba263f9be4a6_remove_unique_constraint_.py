d2780bdf05d032547803180c96a4f15d24e03129
Sunandadadi <sunanda.3094@gmail.com>
2024-09-12 09:50:34 -0400

    autoprune: support multiple organization policies (PROJQUAY-7695)

---
+++ data/migrations/versions/ba263f9be4a6_remove_unique_constraint_.py
@@ -0,0 +1,36 @@
+"""remove unique constraint namespaceautoprunepolicy
+
+Revision ID: ba263f9be4a6
+Revises: a32e17bfad20
+Create Date: 2024-09-09 14:47:59.482614
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "ba263f9be4a6"
+down_revision = "a32e17bfad20"
+
+import sqlalchemy as sa
+
+
+def upgrade(op, tables, tester):
+    # dropping unique index and recreating the index
+    op.drop_index(
+        "namespaceautoprunepolicy_namespace_id",
+        "namespaceautoprunepolicy",
+        if_exists=True,
+    )
+    op.create_index(
+        "namespaceautoprunepolicy_namespace_id",
+        "namespaceautoprunepolicy",
+        ["namespace_id"],
+    )
+
+
+def downgrade(op, tables, tester):
+    op.create_index(
+        "namespaceautoprunepolicy_namespace_id",
+        "namespaceautoprunepolicy",
+        ["namespace_id"],
+        unique=True,
+    )
