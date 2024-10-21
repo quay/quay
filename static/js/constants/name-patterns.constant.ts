bdb860c2a6990e9bdc1ddc946eef6c9d7991fb30
harishsurf <hgovinda@redhat.com>
2024-10-21 13:19:43 -0400

    old ui: update regex pattern for validating team name

--- static/js/constants/name-patterns.constant.ts
+++ static/js/constants/name-patterns.constant.ts
@@ -2,7 +2,7 @@
  * Regex patterns to for validating account names.
  */
 export const NAME_PATTERNS: any = {
-  TEAM_PATTERN: '^[a-z][a-z0-9]+$',
+  TEAM_PATTERN: '^([a-z0-9]+(?:[._-][a-z0-9]+)*)$',
   ROBOT_PATTERN: '^[a-z][a-z0-9_]{1,254}$',
   USERNAME_PATTERN: '^(?=.{2,255}$)([a-z0-9]+(?:[._-][a-z0-9]+)*)$',
 };
