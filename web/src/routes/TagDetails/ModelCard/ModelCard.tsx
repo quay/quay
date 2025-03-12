9ec2cbd085af9cee9fd7b60edc9279dc6180cb56
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2025-03-12 11:59:15 -0400

    ui: oembed to render embeded video in markdown (PROJQUAY-8679)

--- web/src/routes/TagDetails/ModelCard/ModelCard.tsx
+++ web/src/routes/TagDetails/ModelCard/ModelCard.tsx
@@ -1,6 +1,8 @@
 import {PageSection, Divider, TextContent} from '@patternfly/react-core';
 
 import {Remark} from 'react-remark';
+import remarkGfm from 'remark-gfm';
+import oembed from '@agentofuser/remark-oembed';
 
 export function ModelCard(props: ModelCardProps) {
   return (
@@ -8,7 +10,7 @@ export function ModelCard(props: ModelCardProps) {
       <Divider />
       <PageSection>
         <TextContent>
-          <Remark remarkPlugins={[remarkGfm]}>{props.modelCard}</Remark>
+          <Remark remarkPlugins={[remarkGfm, oembed]}>{props.modelCard}</Remark>
         </TextContent>
       </PageSection>
     </>
