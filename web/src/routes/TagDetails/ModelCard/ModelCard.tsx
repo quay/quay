aa3232dd558763adf3b65d1b74fb7898076a2a04
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2025-03-24 17:03:07 -0400

    ui: render tables and embeded links in markdown (PROJQUAY-8673)

--- web/src/routes/TagDetails/ModelCard/ModelCard.tsx
+++ web/src/routes/TagDetails/ModelCard/ModelCard.tsx
@@ -1,16 +1,20 @@
 import {PageSection, Divider, TextContent} from '@patternfly/react-core';
 
-import {Remark} from 'react-remark';
+import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import oembed from '@agentofuser/remark-oembed';
+import rehypeRaw from 'rehype-raw';
 
 export function ModelCard(props: ModelCardProps) {
+  const modelcard = props.modelCard;
+
   return (
     <>
       <Divider />
       <PageSection>
         <TextContent>
-          <Remark remarkPlugins={[remarkGfm, oembed]}>{props.modelCard}</Remark>
+          <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+            {modelcard}
+          </Markdown>
         </TextContent>
       </PageSection>
     </>
