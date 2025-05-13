d3bcbe0610917ba4175efd9fe9b2c8adeb222f45
Sunandadadi <Sunandadadi@users.noreply.github.com>
2025-05-13 10:21:20 -0400

    migration: alter id column type for table manifestblob (#3885)
    * migration: alter id column type for table manifestblob
    
    * adding raw queries

---
+++ data/migrations/versions/e8ed3fb547da_alter_manifestblob_id.py
@@ -0,0 +1,37 @@
+"""alter manifestblob id
+
+Revision ID: e8ed3fb547da
+Revises: 3634f2df3c5b
+Create Date: 2025-05-13 09:45:39.152681
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "e8ed3fb547da"
+down_revision = "3634f2df3c5b"
+
+import sqlalchemy as sa
+
+
+def upgrade(op, tables, tester):
+    bind = op.get_bind()
+    if bind.engine.name == "postgresql":
+        op.execute(
+            """
+            ALTER TABLE manifestblob ALTER COLUMN id TYPE BIGINT;
+        """
+        )
+        op.execute(
+            """
+            ALTER SEQUENCE manifestblob_id_seq AS BIGINT;
+        """
+        )
+        op.execute(
+            """
+            ALTER SEQUENCE manifestblob_id_seq MAXVALUE 9223372036854775807;
+        """
+        )
+
+
+def downgrade(op, tables, tester):
+    pass
