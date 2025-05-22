e500672f25e2782680550f02670dedddc2aedd7a
Marcus Kok <47163063+Marcusk19@users.noreply.github.com>
2025-05-22 14:13:40 -0400

    chore: updating rpms (PROJQUAY-0000) (#3948)
    * updating rpms.lock
    
    * update repo ids to what is in cpe mapping
    
    * update rpms.lock after adding repos

--- hack/generate-rpms.sh
+++ hack/generate-rpms.sh
@@ -4,10 +4,10 @@ DOCKER="docker"
 CONTAINER_DIR="/work"
 BASE_IMAGE="registry.access.redhat.com/ubi8/ubi-minimal:latest"
 
-$DOCKER run -it $BASE_IMAGE cat /etc/yum.repos.d/ubi.repo > ubi.repo
+# $DOCKER run -it $BASE_IMAGE cat /etc/yum.repos.d/ubi.repo > ubi.repo
 
-sed -i '' 's/ubi-9-codeready-builder/codeready-builder-for-ubi-9-$basearch/' ubi.repo
-sed -i '' 's/\[ubi-9/[ubi-9-for-$basearch/' ubi.repo
+# sed -i 's/ubi-9-codeready-builder/codeready-builder-for-ubi-9-$basearch/' ubi.repo
+# sed -i 's/\[ubi-9/[ubi-9-for-$basearch/' ubi.repo
 
 curl https://raw.githubusercontent.com/konflux-ci/rpm-lockfile-prototype/refs/heads/main/Containerfile \
   | $DOCKER build -t localhost/rpm-lockfile-prototype -
