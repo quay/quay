1035873103900ee6162a55fa276f59842ccf63dc
Ivan Bazulic <ibazulic@redhat.com>
2025-07-15 11:58:26 -0400

    App name change __main__ -> init

--- mirror-worker-init/mirror-worker-init.py
+++ mirror-worker-init/mirror-worker-init.py
@@ -5,7 +5,7 @@ from time import sleep
 
 import requests
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("init")
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler = logging.StreamHandler()
