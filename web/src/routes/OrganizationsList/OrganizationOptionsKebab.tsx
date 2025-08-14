149e020d097a0a71117fae0ba82c0783dc6d2310
mfrances <mfrances@redhat.com>
2025-08-14 16:41:18 -0400

    superuser organization action menu commands

--- web/src/routes/OrganizationsList/OrganizationOptionsKebab.tsx
+++ web/src/routes/OrganizationsList/OrganizationOptionsKebab.tsx
@@ -8,20 +8,35 @@ import {
   Tooltip,
 } from '@patternfly/react-core';
 import {CogIcon} from '@patternfly/react-icons';
+import RenameOrganizationModal from './modals/RenameOrganizationModal';
+import DeleteOrganizationModal from './modals/DeleteOrganizationModal';
+import TakeOwnershipModal from './modals/TakeOwnershipModal';
 
 export default function OrganizationOptionsKebab(
   props: OrganizationOptionsKebabProps,
 ) {
   const [isKebabOpen, setIsKebabOpen] = useState<boolean>(false);
+  const [isRenameModalOpen, setIsRenameModalOpen] = useState<boolean>(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+  const [isTakeOwnershipModalOpen, setIsTakeOwnershipModalOpen] =
+    useState<boolean>(false);
 
   const handleMenuItemClick = (action: string) => {
-    console.log(
-      `${action} clicked for ${props.isUser ? 'user' : 'organization'}: ${
-        props.name
-      }`,
-    );
     setIsKebabOpen(false);
-    // TODO: Implement actual actions
+
+    switch (action) {
+      case 'Rename Organization':
+        setIsRenameModalOpen(true);
+        break;
+      case 'Delete Organization':
+        setIsDeleteModalOpen(true);
+        break;
+      case 'Take Ownership':
+        setIsTakeOwnershipModalOpen(true);
+        break;
+      default:
+        console.log(`${action} not yet implemented`);
+    }
   };
 
   const organizationMenuItems = [
@@ -81,32 +96,52 @@ export default function OrganizationOptionsKebab(
   const menuItems = props.isUser ? userMenuItems : organizationMenuItems;
 
   return (
-    <Tooltip content="Options">
-      <Dropdown
-        onSelect={() => setIsKebabOpen(!isKebabOpen)}
-        toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-          <MenuToggle
-            ref={toggleRef}
-            id={`${props.name}-options-toggle`}
-            data-testid={`${props.name}-options-toggle`}
-            variant="plain"
-            onClick={() => setIsKebabOpen(!isKebabOpen)}
-            isExpanded={isKebabOpen}
-          >
-            <CogIcon />
-          </MenuToggle>
-        )}
-        isOpen={isKebabOpen}
-        onOpenChange={(isOpen) => setIsKebabOpen(isOpen)}
-        popperProps={{
-          enableFlip: true,
-          position: 'right',
-        }}
-        shouldFocusToggleOnSelect
-      >
-        <DropdownList>{menuItems}</DropdownList>
-      </Dropdown>
-    </Tooltip>
+    <>
+      <Tooltip content="Options">
+        <Dropdown
+          onSelect={() => setIsKebabOpen(!isKebabOpen)}
+          toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+            <MenuToggle
+              ref={toggleRef}
+              id={`${props.name}-options-toggle`}
+              data-testid={`${props.name}-options-toggle`}
+              variant="plain"
+              onClick={() => setIsKebabOpen(!isKebabOpen)}
+              isExpanded={isKebabOpen}
+            >
+              <CogIcon />
+            </MenuToggle>
+          )}
+          isOpen={isKebabOpen}
+          onOpenChange={(isOpen) => setIsKebabOpen(isOpen)}
+          popperProps={{
+            enableFlip: true,
+            position: 'right',
+          }}
+          shouldFocusToggleOnSelect
+        >
+          <DropdownList>{menuItems}</DropdownList>
+        </Dropdown>
+      </Tooltip>
+
+      {/* Modals */}
+      <RenameOrganizationModal
+        isOpen={isRenameModalOpen}
+        onClose={() => setIsRenameModalOpen(false)}
+        organizationName={props.name}
+      />
+      <DeleteOrganizationModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        organizationName={props.name}
+      />
+      <TakeOwnershipModal
+        isOpen={isTakeOwnershipModalOpen}
+        onClose={() => setIsTakeOwnershipModalOpen(false)}
+        organizationName={props.name}
+        isUser={props.isUser}
+      />
+    </>
   );
 }
 
