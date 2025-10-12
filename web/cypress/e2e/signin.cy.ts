7bfc199905bd06f0c9942cd77bc2cdaa4ca73ce6
Brady Pratt <bpratt@redhat.com>
2025-10-12 01:42:38 -0500

    fixup! ui: reenable signin test (PROJQUAY-5448)
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- web/cypress/e2e/signin.cy.ts
+++ web/cypress/e2e/signin.cy.ts
@@ -7,6 +7,8 @@ describe('Signin page', () => {
 
   it('Succesful signin', () => {
     cy.visit(`/signin`);
+    // Wait for page to fully load and CSRF token to initialize
+    cy.get('button[type=submit]').should('be.visible');
     cy.get('#pf-login-username-id').type('user1');
     cy.get('#pf-login-password-id').type('password');
     cy.get('button[type=submit]').click();
