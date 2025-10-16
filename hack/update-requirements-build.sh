0693587aa5122a9ebe0e19283dcd1464d79dc9b6
Brandon Caton <bcaton@redhat.com>
2025-10-16 12:25:55 -0400

    konflux: update requirements-build dependencies (#4374)

--- hack/update-requirements-build.sh
+++ hack/update-requirements-build.sh
@@ -4,4 +4,5 @@ pip-compile pyproject.toml -o requirements.in --generate-hashes --allow-unsafe
 PIP_CONSTRAINT=constraints.txt python3 pip_find_builddeps.py requirements.in --append --only-write-on-update -o requirements-build.in
 # we need to do some manipulation to remove conflicting versions
 sed -i .back '/setuptools<74/d' requirements-build.in
+sed -i .back '/setuptools_scm<8.0/d' requirements-build.in
 pip-compile requirements-build.in -o requirements-build.txt --allow-unsafe --generate-hashes
