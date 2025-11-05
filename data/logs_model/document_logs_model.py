d999add0528c8c5698ebed70d204f5774d86e3c7
Ryan Wallace <rywallac@redhat.com>
2025-11-05 09:48:27 -0500

    revert(document_logs_model): track_total_hits change
    Leaving track_total_hits as unset since several unit tests rely on this value being unset in their expected responses.

--- data/logs_model/document_logs_model.py
+++ data/logs_model/document_logs_model.py
@@ -269,7 +269,7 @@ class DocumentLogsModel(SharedModel, ActionLogsDataInterface, ElasticsearchLogsM
             performer_id, repository_id, account_id, filter_kinds, index=index
         )
         search = search.sort({"datetime": "desc"}, {"random_id.keyword": "desc"})
-        search = search.extra(size=size, track_total_hits=False)
+        search = search.extra(size=size)
 
         if after_datetime is not None and after_random_id is not None:
             after_datetime_epoch_ms = epoch_ms(after_datetime)
@@ -428,7 +428,7 @@ class DocumentLogsModel(SharedModel, ActionLogsDataInterface, ElasticsearchLogsM
                 "by_date", "date_histogram", field="datetime", interval="day"
             )
             # es returns all buckets when size=0
-            search = search.extra(size=0, track_total_hits=False)
+            search = search.extra(size=0)
             resp = search.execute()
 
         if not resp.aggregations:
