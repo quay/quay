9da9093de07cbf758265ecebeb3490fc76d30580
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-11-19 19:39:36 +0100

    [redhat-3.16] ui: allow for team creation while creating notification (PROJQUAY-9727) (#4573)
    ui: allow for team creation while creating notification (PROJQUAY-9727)
    
    Co-authored-by: bcaton <bcaton@redhat.com>

--- web/src/routes/RepositoryDetails/Settings/NotificationsCreateNotificationQuay.tsx
+++ web/src/routes/RepositoryDetails/Settings/NotificationsCreateNotificationQuay.tsx
@@ -3,18 +3,29 @@ import {NotificationMethod} from 'src/hooks/UseNotificationMethods';
 import {
   ActionGroup,
   Button,
+  Divider,
   FormGroup,
+  SelectGroup,
+  SelectOption,
   TextInput,
 } from '@patternfly/react-core';
+import {UsersIcon} from '@patternfly/react-icons';
 import {useEffect, useState} from 'react';
+import React from 'react';
 import EntitySearch from 'src/components/EntitySearch';
-import {Entity} from 'src/resources/UserResource';
+import {Entity, EntityKind} from 'src/resources/UserResource';
 import {useUpdateNotifications} from 'src/hooks/UseUpdateNotifications';
 import {NotificationEventConfig} from 'src/hooks/UseEvents';
+import {useFetchTeams} from 'src/hooks/UseTeams';
+import Conditional from 'src/components/empty/Conditional';
+import {useOrganizations} from 'src/hooks/UseOrganizations';
 
 export default function CreateQuayNotification(props: CreateQuayNotification) {
   const [title, setTitle] = useState<string>('');
   const [selectedEntity, setSelectedEntity] = useState<Entity>(null);
+  const {usernames} = useOrganizations();
+  const isUserOrganization = usernames.includes(props.org);
+  const {teams} = useFetchTeams(props.org);
   const {
     create,
     successCreatingNotification,
@@ -54,6 +65,40 @@ export default function CreateQuayNotification(props: CreateQuayNotification) {
     }
   }, [errorCreatingNotification]);
 
+  const handleTeamSelect = (teamName: string) => {
+    setSelectedEntity({
+      name: teamName,
+      is_robot: false,
+      kind: EntityKind.team,
+    });
+  };
+
+  const defaultOptions = [
+    <React.Fragment key="default-options">
+      <Conditional if={!isUserOrganization && teams?.length > 0}>
+        <SelectGroup label="Teams" key="teams-group">
+          {teams?.map(({name}) => (
+            <SelectOption data-testid={`${name}-team`} key={name} value={name}>
+              {name}
+            </SelectOption>
+          ))}
+        </SelectGroup>
+        <Divider component="li" key="divider-1" />
+      </Conditional>
+      <Conditional if={!isUserOrganization}>
+        <SelectOption
+          data-testid="create-new-team-btn"
+          key="create-team"
+          component="button"
+          onClick={() => props.openCreateTeamModal()}
+          isFocused
+        >
+          <UsersIcon /> &nbsp; Create team
+        </SelectOption>
+      </Conditional>
+    </React.Fragment>,
+  ];
+
   return (
     <>
       <FormGroup
@@ -65,6 +110,11 @@ export default function CreateQuayNotification(props: CreateQuayNotification) {
           org={props.org}
           onSelect={(e: Entity) => setSelectedEntity(e)}
           onError={() => props.setError('Unable to look up users')}
+          defaultOptions={defaultOptions}
+          onTeamSelect={handleTeamSelect}
+          value={selectedEntity?.name}
+          onClear={() => setSelectedEntity(null)}
+          placeholderText="Search for user"
         />
       </FormGroup>
       <FormGroup fieldId="title" label="Title">
@@ -96,4 +146,5 @@ interface CreateQuayNotification {
   isValidateConfig: () => boolean;
   closeDrawer: () => void;
   setError: (error: string) => void;
+  openCreateTeamModal: () => void;
 }
