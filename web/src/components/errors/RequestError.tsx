886999e1c5c900c8a001f92166a654602cbbb122
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-11-19 16:23:04 +0100

    [redhat-3.16] ui: rendering error for splunk logs (PROJQUAY-6934) (#4564)
    ui: rendering error for splunk logs (PROJQUAY-6934)
    
    Co-authored-by: bcaton <bcaton@redhat.com>

--- web/src/components/errors/RequestError.tsx
+++ web/src/components/errors/RequestError.tsx
@@ -18,15 +18,19 @@ export default function RequestError(props: RequestErrorProps) {
 
   const errorMessage = props.message || getErrorMessageFromUnknown(props.err);
   const message = capitalizeFirstLetter(errorMessage);
+  const title =
+    props.title !== undefined ? props.title : 'Unable to complete request';
 
   return (
     <PageSection variant={PageSectionVariants.light}>
       <EmptyState variant="full">
-        <EmptyStateHeader
-          titleText="Unable to complete request"
-          icon={<EmptyStateIcon icon={ExclamationTriangleIcon} />}
-          headingLevel="h1"
-        />
+        {title && (
+          <EmptyStateHeader
+            titleText={title}
+            icon={<EmptyStateIcon icon={ExclamationTriangleIcon} />}
+            headingLevel="h1"
+          />
+        )}
         <EmptyStateBody>{message}</EmptyStateBody>
         <EmptyStateFooter>
           <Button title="Home" onClick={() => window.location.reload()}>
@@ -41,4 +45,5 @@ export default function RequestError(props: RequestErrorProps) {
 interface RequestErrorProps {
   err?: unknown;
   message?: string;
+  title?: string;
 }
