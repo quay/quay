702dad73f0eb94ec8f45409cdd2ef49376a03d35
Jordi Piriz <jpiriz@redhat.com>
2026-01-18 17:00:23 +0100

    Migration improvements

--- data/migrations/versions/5c69e7934f0f_add_index_on_manifest_subject.py
+++ data/migrations/versions/5c69e7934f0f_add_index_on_manifest_subject.py
@@ -1,22 +1,21 @@
 """Add index on manifest_subject
 
 Revision ID: 5c69e7934f0f
-Revises: 9307c3d604b4
+Revises: 27d0df099ac4
 Create Date: 2026-01-13 10:01:12.710515
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "5c69e7934f0f"
-down_revision = "9307c3d604b4"
+down_revision = "27d0df099ac4"
 
 import sqlalchemy as sa
-from sqlalchemy.engine.reflection import Inspector
 
 
 def upgrade(op, tables, tester):
     bind = op.get_bind()
-    inspector = Inspector.from_engine(bind)
+    inspector = sa.inspect(bind)
     manifest_indexes = inspector.get_indexes("manifest")
     if "manifest_subject" not in [i["name"] for i in manifest_indexes]:
         with op.get_context().autocommit_block():
@@ -30,4 +29,13 @@ def upgrade(op, tables, tester):
 
 
 def downgrade(op, tables, tester):
-    op.drop_index("manifest_subject", table_name="manifest")
+    bind = op.get_bind()
+    inspector = sa.inspect(bind)
+    manifest_indexes = inspector.get_indexes("manifest")
+    if "manifest_subject" in [i["name"] for i in manifest_indexes]:
+        with op.get_context().autocommit_block():
+            op.drop_index(
+                "manifest_subject",
+                table_name="manifest",
+                postgresql_concurrently=True,
+            )
