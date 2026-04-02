f55866a9e14302969b476c30dbfba2310410d2e6
harishsurf <hgovinda@redhat.com>
2026-04-02 17:19:03 -0400

    PROJQUAY-10812: add callback URL validation to export action logs

--- workers/test/test_exportactionlogsworker.py
+++ workers/test/test_exportactionlogsworker.py
@@ -1,28 +1,29 @@
 import json
 import os
-import pytest
-
 from datetime import datetime, timedelta
+from unittest.mock import patch
 
 import boto3
-
-from httmock import urlmatch, HTTMock
+import pytest
+from httmock import HTTMock, urlmatch
 from moto import mock_s3
 
 from app import storage as test_storage
-from data import model, database
+from data import database, model
 from data.logs_model import logs_model
-from storage import S3Storage, StorageContext, DistributedStorage
-from workers.exportactionlogsworker import ExportActionLogsWorker, POLL_PERIOD_SECONDS
-
+from storage import DistributedStorage, S3Storage, StorageContext
 from test.fixtures import *
-
+from workers.exportactionlogsworker import (
+    POLL_PERIOD_SECONDS,
+    ExportActionLogsWorker,
+    ExportResult,
+)
 
 _TEST_CONTENT = os.urandom(1024)
 _TEST_BUCKET = "somebucket"
 _TEST_USER = "someuser"
 _TEST_PASSWORD = "somepassword"
-_TEST_PATH = "some/cool/path"
+_TEST_PATH = "some/path"
 _TEST_CONTEXT = StorageContext("nyc", None, None, None)
 
 
@@ -37,7 +38,7 @@ def storage_engine(request):
             engine = DistributedStorage(
                 {
                     "foo": S3Storage(
-                        _TEST_CONTEXT, "some/path", _TEST_BUCKET, _TEST_USER, _TEST_PASSWORD
+                        _TEST_CONTEXT, _TEST_PATH, _TEST_BUCKET, _TEST_USER, _TEST_PASSWORD
                     )
                 },
                 ["foo"],
@@ -45,7 +46,8 @@ def storage_engine(request):
             yield engine
 
 
-def test_export_logs_failure(initialized_db):
+@patch("workers.exportactionlogsworker.validate_external_registry_url")
+def test_export_logs_failure(mock_ssrf, initialized_db):
     # Make all uploads fail.
     test_storage.put_content("local_us", "except_upload", b"true")
 
@@ -95,7 +97,8 @@ def test_export_logs_failure(initialized_db):
         False,
     ],
 )
-def test_export_logs(initialized_db, storage_engine, has_logs):
+@patch("workers.exportactionlogsworker.validate_external_registry_url")
+def test_export_logs(mock_ssrf, initialized_db, storage_engine, has_logs):
     # Delete all existing logs.
     database.LogEntry3.delete().execute()
 
@@ -149,28 +152,87 @@ def test_export_logs(initialized_db, storage_engine, has_logs):
 
     url = called[0]["exported_data_url"]
 
-    if url.find("http://localhost:5000/exportedlogs/") == 0:
-        storage_id = url[len("http://localhost:5000/exportedlogs/") :]
-    else:
-        assert url.find("https://somebucket.s3.amazonaws.com/some/path/exportedactionlogs/") == 0
-        storage_id, _ = url[
-            len("https://somebucket.s3.amazonaws.com/some/path/exportedactionlogs/") :
-        ].split("?")
+    # Not direct download url from storage
+    if url != storage_engine.get_direct_download_url(
+        storage_engine.preferred_locations, "exportedactionlogs"
+    ):
+        if url.find("http://localhost:5000/exportedlogs/") == 0:
+            storage_id = url[len("http://localhost:5000/exportedlogs/") :]
+        else:
+            assert (
+                url.find(
+                    "https://somebucket.s3.amazonaws.com/" + _TEST_PATH + "/exportedactionlogs/"
+                )
+                == 0
+            )
+            storage_id, _ = url[
+                len("https://somebucket.s3.amazonaws.com/" + _TEST_PATH + "/exportedactionlogs/") :
+            ].split("?")
 
-    created = storage_engine.get_content(
-        storage_engine.preferred_locations, "exportedactionlogs/" + storage_id
-    )
-    created_json = json.loads(created)
+        created = storage_engine.get_content(
+            storage_engine.preferred_locations, "exportedactionlogs/" + storage_id
+        )
+        created_json = json.loads(created)
 
-    if has_logs:
-        found = set()
-        for log in created_json["logs"]:
-            if log.get("terminator"):
-                continue
+        if has_logs:
+            found = set()
+            for log in created_json["logs"]:
+                if log.get("terminator"):
+                    continue
 
-            found.add(log["metadata"]["index"])
+                found.add(log["metadata"]["index"])
 
-        for index in range(-10, 10):
-            assert index in found
-    else:
-        assert created_json["logs"] == [{"terminator": True}]
+            for index in range(-10, 10):
+                assert index in found
+        else:
+            assert created_json["logs"] == [{"terminator": True}]
+
+
+def _make_job_details(callback_url, callback_email=None):
+    """Helper to build a minimal job_details dict for _report_results tests."""
+    return {
+        "export_id": "test-export-id",
+        "start_time": "01/01/2026",
+        "end_time": "01/31/2026",
+        "namespace_name": "devtable",
+        "repository_name": "simple",
+        "callback_url": callback_url,
+        "callback_email": callback_email,
+    }
+
+
+class TestExportLogsWorkerCallbackValidation:
+    """Tests for callback URL validation in the export action logs worker."""
+
+    def test_blocked_callback_skips_post(self, initialized_db):
+        """Worker skips POST when callback URL points to a blocked address."""
+        worker = ExportActionLogsWorker(None)
+        called = [False]
+
+        @urlmatch(netloc=r"169.254.169.254")
+        def handle_request(url, request):
+            called[0] = True
+            return {"status_code": 200, "content": "{}"}
+
+        job_details = _make_job_details("http://169.254.169.254/latest/meta-data")
+
+        with HTTMock(handle_request):
+            worker._report_results(
+                job_details, ExportResult.SUCCESSFUL_EXPORT, "http://example.com/export"
+            )
+
+        assert not called[0]
+
+    def test_blocked_callback_still_sends_email(self, initialized_db):
+        """Worker skips POST but still sends email when callback URL is blocked."""
+        worker = ExportActionLogsWorker(None)
+        job_details = _make_job_details(
+            "http://169.254.169.254/latest/meta-data",
+            callback_email="test@example.com",
+        )
+
+        with patch("workers.exportactionlogsworker.send_logs_exported_email") as mock_email:
+            worker._report_results(
+                job_details, ExportResult.SUCCESSFUL_EXPORT, "http://example.com/export"
+            )
+            mock_email.assert_called_once()
