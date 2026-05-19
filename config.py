ff1b5ab4815fcece829db699925a2d5f4bd6693e
dronenb <dronenb@users.noreply.github.com>
2026-05-19 16:19:58 -0400

    feat(oauth): allow any authenticated user to authorize OAuth apps
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- config.py
+++ config.py
@@ -54,6 +54,7 @@ CLIENT_WHITELIST = [
     "FOOTER_LINKS",
     "UI_DELAY_AFTER_WRITE_SECONDS",
     "FEATURE_ASSIGN_OAUTH_TOKEN",
+    "FEATURE_PUBLIC_OAUTH_APPS",
     "FEATURE_IMAGE_EXPIRY_TRIGGER",
     "FEATURE_AUTO_PRUNE",
     "DEFAULT_NAMESPACE_AUTOPRUNE_POLICY",
@@ -998,6 +999,7 @@ class DefaultConfig(ImmutableConfig):
     ROBOTS_WHITELIST: Optional[List[str]] = []
 
     FEATURE_ASSIGN_OAUTH_TOKEN = True
+    FEATURE_PUBLIC_OAUTH_APPS = True
     DEFAULT_NAMESPACE_AUTOPRUNE_POLICY: Optional[Dict[str, str]] = None
 
     # Allows users to set up notifications on image expiry, can remove flag once feature is tested
