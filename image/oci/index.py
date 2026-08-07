68e7bc1aceec51789bf29b377810cc66a15c15ae
Ryan Wallace <rywallac@redhat.com>
2026-08-07 16:45:28 -0400

    NO-ISSUE: fix: platform KeyError in _get_legacy_manifest

--- image/oci/index.py
+++ image/oci/index.py
@@ -391,9 +391,11 @@ class OCIIndex(ManifestListInterface):
         if none or error.
         """
         for manifest_ref in self.manifests(content_retriever):
-            platform = manifest_ref._manifest_data[INDEX_PLATFORM_KEY]
-            architecture = platform[INDEX_ARCHITECTURE_KEY]
-            os = platform[INDEX_OS_KEY]
+            platform = manifest_ref._manifest_data.get(INDEX_PLATFORM_KEY)
+            if platform is None:
+                continue
+            architecture = platform.get(INDEX_ARCHITECTURE_KEY)
+            os = platform.get(INDEX_OS_KEY)
             if architecture != "amd64" or os != "linux":
                 continue
 
