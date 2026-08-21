e2a2c7c8ca9d36266452c4ad6889ff10f21fecb7
Shaon <shossain@redhat.com>
2026-08-21 11:38:54 -0700

    PROJQUAY-12772: feat(config): add digest algorithm allowlist
    # Conflicts:
    #	data/secscan_model/secscan_v4_model_v2.py

--- data/model/user.py
+++ data/model/user.py
@@ -138,7 +138,7 @@ def create_user_noverify(
         # ID to ensure that the database consistency check remains intact.
         email = email or str(uuid.uuid4())
 
-    (username_valid, username_issue) = validate_username(username)
+    username_valid, username_issue = validate_username(username)
     if not username_valid:
         raise InvalidUsernameException("Invalid namespace %s: %s" % (username, username_issue))
 
@@ -278,7 +278,7 @@ def get_user_prompts(user):
 
 
 def change_username(user_id, new_username):
-    (username_valid, username_issue) = validate_username(new_username)
+    username_valid, username_issue = validate_username(new_username)
     if not username_valid:
         raise InvalidUsernameException("Invalid username %s: %s" % (new_username, username_issue))
 
@@ -353,7 +353,7 @@ def update_enabled(user, set_enabled):
 
 
 def create_robot(robot_shortname, parent, description="", unstructured_metadata=None, token=None):
-    (username_valid, username_issue) = validate_username(robot_shortname)
+    username_valid, username_issue = validate_username(robot_shortname)
     if config.app_config.get("ROBOTS_DISALLOW", False):
         msg = "Robot accounts have been disabled. Please contact your administrator."
         raise InvalidRobotException(msg)
