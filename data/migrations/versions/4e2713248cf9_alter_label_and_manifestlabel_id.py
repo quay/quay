7b8d22f901409e27396219543aa7dc2eb8a11f5e
Shaon <shossain@redhat.com>
2026-09-16 10:58:48 -0700

    QUAYIO-2239: make sequence migration idempotent

--- data/migrations/versions/4e2713248cf9_alter_label_and_manifestlabel_id.py
+++ data/migrations/versions/4e2713248cf9_alter_label_and_manifestlabel_id.py
@@ -14,6 +14,23 @@ import sqlalchemy as sa
 from sqlalchemy.engine.reflection import Inspector
 
 
+def _get_sequence_state(bind, sequence_name):
+    return (
+        bind.execute(
+            sa.text("""
+                SELECT
+                    format_type(seqtypid, NULL) AS data_type,
+                    seqmax AS max_value
+                FROM pg_sequence
+                WHERE seqrelid = to_regclass(:sequence_name)
+                """),
+            {"sequence_name": sequence_name},
+        )
+        .mappings()
+        .one()
+    )
+
+
 def upgrade(op, tables, tester):
     bind = op.get_bind()
     inspector = Inspector.from_engine(bind)
@@ -30,22 +47,32 @@ def upgrade(op, tables, tester):
             op.execute("""
                 ALTER TABLE label ALTER COLUMN id TYPE BIGINT;
             """)
-        op.execute("""
-            ALTER SEQUENCE label_id_seq AS BIGINT;
-        """)
-        op.execute("""
-            ALTER SEQUENCE label_id_seq MAXVALUE 9223372036854775807;
-        """)
+
+        label_sequence = _get_sequence_state(bind, "label_id_seq")
+        if label_sequence["data_type"].lower() != "bigint":
+            op.execute("""
+                ALTER SEQUENCE label_id_seq AS BIGINT;
+            """)
+        if label_sequence["max_value"] != 9223372036854775807:
+            op.execute("""
+                ALTER SEQUENCE label_id_seq MAXVALUE 9223372036854775807;
+            """)
+
         if manifestlabel_columns["id"] != "bigint":
             op.execute("""
                 ALTER TABLE manifestlabel ALTER COLUMN id TYPE BIGINT;
             """)
-        op.execute("""
-            ALTER SEQUENCE manifestlabel_id_seq AS BIGINT;
-        """)
-        op.execute("""
-            ALTER SEQUENCE manifestlabel_id_seq MAXVALUE 9223372036854775807;
-        """)
+
+        manifestlabel_sequence = _get_sequence_state(bind, "manifestlabel_id_seq")
+        if manifestlabel_sequence["data_type"].lower() != "bigint":
+            op.execute("""
+                ALTER SEQUENCE manifestlabel_id_seq AS BIGINT;
+            """)
+        if manifestlabel_sequence["max_value"] != 9223372036854775807:
+            op.execute("""
+                ALTER SEQUENCE manifestlabel_id_seq MAXVALUE 9223372036854775807;
+            """)
+
         if manifestlabel_columns["label_id"] != "bigint":
             op.execute("""
                 ALTER TABLE manifestlabel ALTER COLUMN label_id TYPE BIGINT;
