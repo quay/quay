31fed78be15649921979da99d9cd7ea2553056a3
Ivan Bazulic <ibazulic@redhat.com>
2026-09-21 18:44:12 +0200

    PROJQUAY-11278: fix: Properly parse region_name on RadosGWStorage validation Previously, the region_name parameter was not read by the validator during storage validation, Minio tried to figure out what the region is automatically. Certain storage engines (like garage) reported a `400 Bad Request` because the initial request was malformed due to missing region. This PR adds explicit validation of the region name with RadosGWStorage driver. If region name is not present, validator will fall back to established behavior.

--- config-tool/pkg/lib/fieldgroups/distributedstorage/distributedstorage_test.go
+++ config-tool/pkg/lib/fieldgroups/distributedstorage/distributedstorage_test.go
@@ -11,7 +11,15 @@ import (
 // TestValidateSchema tests the ValidateSchema function
 func TestValidateDistributedStorage(t *testing.T) {
 
-	var config = []byte(`DISTRIBUTED_STORAGE_CONFIG:
+	// Define test data
+	var tests = []struct {
+		name   string
+		config []byte
+		want   string
+	}{
+
+		{name: "MissingStorageConfig", config: []byte(``), want: "invalid"},
+		{name: "CorrectStorageConfig", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
   local_us:
   - RadosGWStorage
   - access_key: X
@@ -20,16 +28,56 @@ func TestValidateDistributedStorage(t *testing.T) {
     is_secure: true
     port: 443
     secret_key: X
-    storage_path: /datastorage/registry`)
-
-	// Define test data
-	var tests = []struct {
-		name   string
-		config map[string]interface{}
-		want   string
-	}{
-
-		{name: "MissingStorageConfig", config: map[string]interface{}{}, want: "invalid"},
+    storage_path: /datastorage/registry`), want: "valid"},
+		{name: "StorageWithRegionSet", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
+  local_us:
+  - RadosGWStorage
+  - access_key: X
+    bucket_name: quay-datastore
+    hostname: jonathan-registry.com
+    is_secure: true
+    port: 443
+    secret_key: X
+    region_name: someregion
+    storage_path: /datastorage/registry`), want: "valid"},
+		{name: "StorageWithoutPortSet", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
+  local_us:
+  - RadosGWStorage
+  - access_key: X
+    bucket_name: quay-datastore
+    hostname: jonathan-registry.com
+    is_secure: true
+    secret_key: X
+    region_name: someregion
+    storage_path: /datastorage/registry`), want: "valid"},
+		{name: "StorageWithIPAddressSet", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
+  local_us:
+  - RadosGWStorage
+  - access_key: X
+    bucket_name: quay-datastore
+    hostname: 1.2.3.4
+    is_secure: true
+    port: 443
+    secret_key: X
+    region_name: someregion
+    storage_path: /datastorage/registry`), want: "valid"},
+		{name: "StorageWithNoHostnameSet", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
+  local_us:
+  - RadosGWStorage
+  - access_key: X
+    bucket_name: quay-datastore
+    is_secure: true
+    port: 443
+    secret_key: X
+    region_name: someregion
+    storage_path: /datastorage/registry`), want: "invalid"},
+		{name: "StorageWithNoAccessParamsSet", config: []byte(`DISTRIBUTED_STORAGE_CONFIG:
+  local_us:
+  - RadosGWStorage
+  - bucket_name: quay-datastore
+    hostname: jonathan-registry.com
+    region_name: someregion
+    storage_path: /datastorage/registry`), want: "invalid"},
 	}
 
 	// Iterate through tests
@@ -40,7 +88,7 @@ func TestValidateDistributedStorage(t *testing.T) {
 
 			// Load config into struct
 			var conf map[string]interface{}
-			if err := yaml.Unmarshal(config, &conf); err != nil {
+			if err := yaml.Unmarshal(tt.config, &conf); err != nil {
 				fmt.Println(err.Error())
 			}
 
